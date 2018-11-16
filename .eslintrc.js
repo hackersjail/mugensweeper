b9d509da86ee2ccde7288aa37f8b47d9c04b4924
hi85 <33993836+hi85gh@users.noreply.github.com>
2018-11-17 02:25:02 +0900

    Add Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,4 +15,12 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
+  overrides: [
+    {
+      files: ['**/tests/**/*.test.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
