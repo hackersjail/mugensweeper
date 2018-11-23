24c8cb14a2b50626f9f3300eefd62e32f499869e
NaomiRennie <naomi.lily.405@gmail.com>
2018-11-24 08:42:32 +0900

    fix: line

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 
 router.use('/example', require('./example.js'));
-
-// fieldにつなぐ
-
 router.use('/rennie/field', require('./rennie/field.js'));
 
 module.exports = router;
