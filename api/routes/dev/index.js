b7bc8fec4b7314819962ee6851e4b00c5784eadb
NaomiRennie <naomi.lily.405@gmail.com>
2018-11-24 07:55:40 +0900

    field apiのテストまで完了

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -2,4 +2,8 @@ const router = require('express').Router();
 
 router.use('/example', require('./example.js'));
 
+// fieldにつなぐ
+
+router.use('/rennie/field', require('./rennie/field.js'));
+
 module.exports = router;
