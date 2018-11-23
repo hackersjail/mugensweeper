aa104ea82a0c8a1d6552b59229c58d29209e1194
三島明恵 <myunghye.kor@gmail.com>
2018-11-24 08:32:08 +0900

    index.jsの競合を解消

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use('/example', require('./example.js'));
+router.use('/matsuda/field', require('./matsuda/field.js'));
 router.use('/mishima', require('./mishima/field.js'));
 
 module.exports = router;
