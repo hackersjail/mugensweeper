e395a239023f405bc405588932d24aebb1df4f44
Mitsuhide Matsuda <genbu2011@live.jp>
2018-11-24 07:55:37 +0900

    test: first field API

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/example', require('./example.js'));
+router.use('/matsuda/field', require('./matsuda/field.js'));
 
 module.exports = router;
