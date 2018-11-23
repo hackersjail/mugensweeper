1a4ce32aa125216b29de98f92e873895694d1da0
hiroshima0422 <hiroshima0422@gmail.com>
2018-11-24 07:56:55 +0900

    fieldapi

---
+++ api/tests/routes/hiro/field.test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const app = require('../../../routes/app.js');
+
+describe('Example of Jest using Express', () => {
+  it('Evaluate the requested query', async () => {
+    // Given
+    //const given = 'mugensweeper';
+
+    console.log('aaa');
+    // When
+    const { body } = await chai.request(app).get(`/dev/hiro/field`);
+    console.log(body[1].user.name);
+    // Then
+    expect(body).toHaveLength(2);
+    expect(body[0]).toMatchObject({x:0,y:0});
+
+    //expect(res.text).toMatch(/grapefruit/).toBe(given);
+  });
+});
