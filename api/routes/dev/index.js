735806fc9d8cb35a65f2010583d7587682cac46b
Mitsuhide Matsuda <genbu2011@live.jp>
2018-11-24 09:10:26 +0900

    コメント追加

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/example', require('./example.js'));
 router.use('/matsuda/field', require('./matsuda/field.js'));
+// コンフリクトを発生させるためだけのコメント
 
 module.exports = router;
