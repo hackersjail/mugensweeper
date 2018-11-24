7a82d49093f76757b319d3b6fa51a641ffec577e
shiratsuchisatoshi <white.earth.3104@gmail.com>
2018-11-24 11:41:47 +0900

    conflict修正

--- api/routes/dev/shiratsuchi/field.js
+++ api/routes/dev/shiratsuchi/field.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 router.route('/').get((req, res) => {
   // res.send(req.query.string);
-// const res ={x:0,y:0};
+  // const res ={x:0,y:0};
 
-let x=[{x:0,y:0}];
-// res.json(req.query.string);
-res.json(x);
+  const x = [{ x: 0, y: 0 }];
+  // res.json(req.query.string);
+  res.json(x);
   // console.log(req.query.string);
 });
 
---
+++ api/routes/dev/shiratsuchi/quiz.js
@@ -0,0 +1,3 @@
+
+
+module.exports = (n) => n**2*3+4;
