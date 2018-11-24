b94bb299e0fe67272888fd110c7667d91da9f7cc
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-11-24 09:04:07 +0900

    revised files

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
 router.use('/example', require('./example.js'));
-router.use('/miyamoto', require('./miyamoto.js'));
+router.use('/miyamoto/field', require('./miyamoto/field.js'));
 
 module.exports = router;
--- api/routes/dev/miyamoto.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.use('/field', require('./miyamoto/field.js'));
-
-module.exports = router;
