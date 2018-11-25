43f0a6143d1ccf8e5a1e135fa3bedf75a9100869
hiroshima0422 <hiroshima0422@gmail.com>
2018-11-26 07:20:28 +0900

    hiro12

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -5,6 +5,5 @@ router.use('/rennie/field', require('./rennie/field.js'));
 router.use('/eto/field', require('./eto/field.js'));
 router.use('/miyamoto/field', require('./miyamoto/field.js'));
 router.use('/matsuda/field', require('./matsuda/field.js'));
-router.use('/hiro/field', require('./hiro/field.js'));
 
 module.exports = router;
