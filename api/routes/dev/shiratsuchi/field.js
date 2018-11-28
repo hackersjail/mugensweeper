03b8a7ef176131e599400c4af886188a5fc1ef64
shiratsuchisatoshi <white.earth.3104@gmail.com>
2018-11-28 17:54:40 +0900

    2018/11/28

--- api/routes/dev/shiratsuchi/field.js
+++ api/routes/dev/shiratsuchi/field.js
@@ -1,13 +1,7 @@
 const router = require('express').Router();
 
 router.route('/').get((req, res) => {
-  // res.send(req.query.string);
-  // const res ={x:0,y:0};
-
-  const x = [{ x: 0, y: 0 }];
-  // res.json(req.query.string);
-  res.json(x);
-  // console.log(req.query.string);
+  res.json([{ x: 0, y: 0 }]);
 });
 
 module.exports = router;
