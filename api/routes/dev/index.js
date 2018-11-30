0ce7501c1f524d845f5c889a8e928f19245387d4
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-12-01 00:54:29 +0900

    理解状況確認のため再テスト実施

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -5,5 +5,6 @@ router.use('/rennie/field', require('./rennie/field.js'));
 router.use('/eto/field', require('./eto/field.js'));
 router.use('/miyamoto/field', require('./miyamoto/field.js'));
 router.use('/matsuda/field', require('./matsuda/field.js'));
+router.use('/miyamoto2/field', require('./miyamoto2/field.js'));
 
 module.exports = router;
---
+++ api/tests/routes/dev/miyamoto2/field.test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const app = require('../../../../routes/app.js');
+
+describe('field APIについてのテスト', () => {
+  it('最初のfield情報を取得する', async () => {
+    // Given
+
+    // When
+    const { body } = await chai.request(app).get('/dev/miyamoto2/field');
+
+    // Then
+    expect(body).toHaveLength(1);
+    expect(body[0]).toMatchObject({ x: 0, y: 0 });
+  });
+});
