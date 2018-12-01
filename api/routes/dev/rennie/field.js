415340d7073802f141594843d452b11bb1c5d5a3
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-01 13:33:02 +0900

    pass first test

--- api/routes/dev/rennie/field.js
+++ api/routes/dev/rennie/field.js
@@ -1,8 +1,31 @@
 const router = require('express').Router();
 
-const field = [{ x: 0, y: 0 }];
 router.route('/').get((req, res) => {
-  res.send(field);
+  res.send([
+    {
+      x: 0,
+      y: 0,
+    },
+    // ]);
+    // // res.send([
+    // {
+    //   x: 1,
+    //   y: 3,
+    // },
+  ]);
+});
+
+router.route('/').post((req, res) => {
+  res.send([
+    {
+      x: 0,
+      y: 0,
+    },
+    {
+      x: +req.body.x,
+      y: +req.body.y,
+    },
+  ]);
 });
 
 module.exports = router;
