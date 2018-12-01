12be857c992c2161ae7eb02ffa2e24c1d23a6f8d
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-12-01 09:01:13 +0900

    block用の記述を追加しました

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -4,6 +4,7 @@ router.use('/example', require('./example.js'));
 router.use('/rennie/field', require('./rennie/field.js'));
 router.use('/eto/field', require('./eto/field.js'));
 router.use('/miyamoto/field', require('./miyamoto/field.js'));
+router.use('/miyamoto/block', require('./miyamoto/block.js'));
 router.use('/matsuda/field', require('./matsuda/field.js'));
 
 module.exports = router;
