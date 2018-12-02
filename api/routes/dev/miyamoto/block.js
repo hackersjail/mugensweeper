22851718a34790082835491709a6091020fb0339
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-12-03 06:53:31 +0900

    不要なソースや変数の型を修正

--- api/routes/dev/miyamoto/block.js
+++ api/routes/dev/miyamoto/block.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-let state = ''; // 試合中の盤面情報
+let state = {}; // 試合中の盤面情報
 
 // 盤面初期化用の関数
 const reset = () => {
@@ -8,21 +8,11 @@ const reset = () => {
   const initState = {
     client: [{ x: 0, y: 0 }],
   };
-
   // 盤面情報を初期化
-  if (state === '') {
-    state = Object.assign({}, initState);
-  } else {
-    // **********************
-    // 現在の盤面情報の削除処理
-    // **********************
-    for (let c = 0; c < state.client.length; c += 1) {
-      delete state.client[c].x;
-      delete state.client[c].y;
-    }
-    state = Object.assign({}, initState);
-  }
+  state = Object.assign({}, initState);
 };
+
+reset();
 
 router
   .route('/')
@@ -30,7 +20,6 @@ router
     const x = +req.body.x;
     const y = +req.body.y;
     let flag = false;
-    if (state === '') reset();
     for (let i = 0; i < state.client.length; i += 1) {
       if (state.client[i].x !== x && state.client[i].y !== y) flag = true;
     }
