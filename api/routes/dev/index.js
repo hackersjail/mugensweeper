64b1079039cebe00ff1a00f7bba46c15f4cd4fd5
hiroshima0422 <hiroshima0422@gmail.com>
2018-12-04 05:30:39 +0900

    hiro23

--- api/routes/dev/index.js
+++ api/routes/dev/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
 router.use('/example', require('./example.js'));
-router.use('/rennie/field', require('./rennie/field.js'));
-router.use('/eto/field', require('./eto/field.js'));
-router.use('/miyamoto/field', require('./miyamoto/field.js'));
 router.use('/matsuda/field', require('./matsuda/field.js'));
+router.use('/hiro/field', require('./hiro/field.js'));
 
 module.exports = router;
