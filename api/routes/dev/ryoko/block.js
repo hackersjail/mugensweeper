43f4987031d16906e31957a65e832149034d9ba6
lilycoco <ry.sep27@gmail.com>
2018-12-04 08:27:47 +0900

    重複不可テスト追加"

--- api/routes/dev/ryoko/block.js
+++ api/routes/dev/ryoko/block.js
@@ -4,7 +4,14 @@ const field = [{ x: 0, y: 0 }];
 
 router.route('/').post((req, res) => {
   if (req.body.x) {
-    field.push({ x: Number(req.body.x), y: Number(req.body.y) });
+    for (let i = 0; i < field.length; i += 1) {
+      // console.log(field[i].x, req.body.x, field[i].y, req.body.y);
+      if (field[i].x === Number(req.body.x) && field[i].y === Number(req.body.y)) {
+        break;
+      } else if (i === field.length - 1) {
+        field.push({ x: Number(req.body.x), y: Number(req.body.y) });
+      }
+    }
   }
   res.json(field);
 });
