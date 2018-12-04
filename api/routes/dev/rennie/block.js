7d8d4b828a6d4d055411c84895cce93e88edd472
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-04 20:42:13 +0900

    ランダムな複数の数字のテストまで

--- api/routes/dev/rennie/block.js
+++ api/routes/dev/rennie/block.js
@@ -2,16 +2,31 @@ const router = require('express').Router();
 
 // field testのぶん
 
+// router.route('/').post((req, res) => {
+//   res.json([
+//     {
+//       x: 0,
+//       y: 0,
+//     },
+//     {
+//       x: +req.body.x,
+//       y: +req.body.y,
+//     },
+//   ]);
+// });
+
+// block テスト一番め
+const field = [{ x: 0, y: 0 }];
+
 router.route('/').post((req, res) => {
-  res.json([
-    {
-      x: 0,
-      y: 0,
-    },
-    {
-      x: +req.body.x,
-      y: +req.body.y,
-    },
-  ]);
+  if (req.body.x) {
+    field.push({ x: +req.body.x, y: +req.body.y });
+  }
+  res.json(field);
 });
+router.route('/').delete((req, res) => {
+  field.length = 1;
+  res.json(field);
+});
+
 module.exports = router;
