f62b78c1e4885fef79ed4329470909275392e88f
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-07 22:57:08 +0900

    conflict

--- api/routes/dev/rennie/block.js
+++ api/routes/dev/rennie/block.js
@@ -62,10 +62,10 @@ router.route('/').post((req, res) => {
   res.json(field);
 });
 
-router.route('/').delete((req, res) => {
-  field.length = 1;
-  res.json(field);
-});
+// router.route('/').delete((req, res) => {
+//   field.length = 1;
+//   res.json(field);
+// });
 
 // const field = [{ x: 0, y: 0 }];
 // const openedBlock = [];
@@ -129,6 +129,88 @@ router.route('/').delete((req, res) => {
 
 //     if (openedBlock.length === 0) {
 //       for (let i = 0; i < tempBlock.length; i++) {
+//         openedBlock.push({ x: tempBlock[i][0], y: tempBlogk[i][1] });
+//       }
+//     } else {
+//       openedBlock.concat(tempBlock);
+//     }
+//     console.log('kore', tempBlock);
+//     console.log('kotti', openedBlock);
+//     console.log('GGGGGG');
+
+//     tempBlock.empty();
+//   }
+//   console.log('boms2', boms);
+//   // res.json(boms);
+// });
+
+// router.route('/').delete((req, res) => {
+//   field.length = 1;
+//   res.json(field);
+// });
+
+// const field = [{ x: 0, y: 0 }];
+// const openedBlock = [];
+// const boms = [];
+// const tempBlock = [];
+
+// router.route('/').post((req, res) => {
+//   // const openedBlock = [];
+//   // const boms = [];
+//   // const tempBlock = [];
+//   if (req.body.x) {
+//     const directions = [[0, -1], [1, 0], [0, 1], [-1, 0], [-1, -1], [1, 1], [-1, 1], [1, -1]];
+//     for (i = 0; i < directions.length; i++) {
+//       const a = Number(directions[i][0]);
+//       const b = Number(directions[i][1]);
+//       let m = field.length;
+//       while (m >= 0) {
+//         if (field[m - 1].y + b === +req.body.y && field[m - 1].x + a === +req.body.x) {
+//           field.push({ x: +req.body.x, y: +req.body.y });
+//           if (tempBlock.length === 0) {
+//             for (i = 0; i < directions.length; i++) {
+//               const c = Number(directions[i][0]);
+//               const d = Number(directions[i][1]);
+//               const e = field[m - 1].x + c;
+//               const f = field[m - 1].y + d;
+//               tempBlock.push({ x: e, y: f });
+//             }
+//           } else {
+//             for (i = 0; i < directions.length; i++) {
+//               const c = Number(directions[i][0]);
+//               const d = Number(directions[i][1]);
+//               const e = field[m - 1].x + c;
+//               const f = field[m - 1].y + d;
+//               console.log(tempBlock, 'kokoi');
+//               tempBlock.filter((a, b, c) => {
+//                 if (c[b].x === e && c[b].y === f) {
+//                 } else {
+//                   console.log('BBBBBBBBBBBB');
+//                   tempBlock.push({ x: e, y: f });
+//                 }
+//               });
+//             }
+//           }
+//           m -= 1;
+//           break;
+//         }
+//         m -= 1;
+//         break;
+//       }
+//     }
+//     console.log(tempBlock);
+//     const bomCount = tempBlock.length * 0.375;
+//     for (i = 0; i < bomCount; i++) {
+//       const number = Math.floor(tempBlock.length * Math.random());
+//       const tempx = tempBlock[number].x;
+//       const tempy = tempBlock[number].y;
+//       boms.push({ x: tempx, y: tempy });
+//     }
+//     console.log('boms3', boms);
+//     res.json(boms);
+
+//     if (openedBlock.length === 0) {
+//       for (i = 0; i < tempBlock.length; i++) {
 //         openedBlock.push({ x: tempBlock[i][0], y: tempBlogk[i][1] });
 //       }
 //     } else {
