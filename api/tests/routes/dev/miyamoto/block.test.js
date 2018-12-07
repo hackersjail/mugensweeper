b4b24ea6634107a0ee35a20b9818ff0075307723
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-12-08 01:50:43 +0900

    不要なソースを削除

--- api/tests/routes/dev/miyamoto/block.test.js
+++ api/tests/routes/dev/miyamoto/block.test.js
@@ -38,44 +38,6 @@ describe('前のゲーム情報のリセット処理、および、リクエス�
     expect(body).toEqual(expect.arrayContaining([initialBlock()]));
   });
 
-  // 周囲８方向のみ開けるテストで必ず落ちるのでコメントアウト
-  // it('ランダムに複数Postした座標が返り値に追加される', async () => {
-  //   // 前のテストのBlockをサーバーから消しておく
-  //   await chai.request(app).delete('/dev/miyamoto/block');
-
-  //   // Given
-  //   const positions = [];
-  //   const count = Math.floor(5 * Math.random()) + 5;
-  //   let cnt = 0;
-  //   while (cnt < count) {
-  //     const tmp = {
-  //       // eslint-disable-line
-  //       x: Math.floor(10000 * Math.random()),
-  //       y: Math.floor(10000 * Math.random()),
-  //     };
-
-  //     if (positions.indexOf(tmp) === -1) {
-  //       positions.push(tmp);
-  //       cnt += 1;
-  //     }
-  //   }
-
-  //   // When
-  //   let lastBody;
-  //   for (let i = 0; i < positions.length; i += 1) {
-  //     const { body } = await chai
-  //       .request(app)
-  //       .post('/dev/miyamoto/block')
-  //       .set('content-type', 'application/x-www-form-urlencoded')
-  //       .send(positions[i]);
-  //     lastBody = body;
-  //   }
-
-  //   // Then
-  //   expect(lastBody).toHaveLength(count + 1);
-  //   expect(lastBody).toEqual(expect.arrayContaining([initialBlock(), ...positions]));
-  // });
-
   it('同じ座標にはpostしても登録されない', async () => {
     // 前のテストのBlockをサーバーから消しておく
     await chai.request(app).delete('/dev/miyamoto/block');
