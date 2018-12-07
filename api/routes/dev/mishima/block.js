0c11e06bcc48040824ed1b0942aedcfa4b3d25d0
三島明恵 <myunghye.kor@gmail.com>
2018-12-08 04:43:02 +0900

    八方面開けるを実装

--- api/routes/dev/mishima/block.js
+++ api/routes/dev/mishima/block.js
@@ -9,8 +9,23 @@ router.route('/').delete((req, res) => {
 
 router.route('/').post((req, res) => {
   const fieldIdx = field.findIndex((elem) => elem.x === +req.body.x && elem.y === +req.body.y);
+  // 周囲の相対的座標
+  const directions = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]];
 
-  if (fieldIdx === -1) {
+  const arroundCounter = directions.reduce((acc, val) => {
+    const arroundX = +req.body.x + val[0];
+    const arroundY = +req.body.y + val[1];
+    let counter = 0;
+    for (let i = 0; i < field.length; i += 1) {
+      if (field[i].x === arroundX && field[i].y === arroundY) {
+        counter += 1;
+      }
+    }
+    return acc + counter;
+  }, 0);
+
+  // 重複していなければpushする
+  if (fieldIdx === -1 && arroundCounter > 0) {
     field.push({ x: +req.body.x, y: +req.body.y });
   }
 
