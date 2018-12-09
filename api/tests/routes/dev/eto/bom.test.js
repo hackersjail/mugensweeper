ae7dae3a3989775e28523261e6caef30d1d7d029
Takuto Eto <aoitele@gmail.com>
2018-12-09 12:57:56 +0900

    prettier動作確認用のコミットです

---
+++ api/tests/routes/dev/eto/bom.test.js
@@ -0,0 +1,21 @@
+const bomMap = require('../../../../models/eto/bomMap.js');
+
+const directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+describe('爆弾マップのテスト', () => {
+  it('原点の周囲の爆弾マップが帰ってくる', () => {
+    // GIVEN
+    const position = { x: 0, y: 0 };
+    const bomCount = Math.ceil(Math.random() * 3);
+
+    // WHEN
+    const map = bomMap.initSet(bomCount, position);
+
+    // THEN
+    const mapMatcher = directions.map(([x, y]) => ({ x, y }));
+    const bomReturn = map.filter(({ bom }) => bom).length;
+    expect(map).toHaveLength(8);
+    expect(map.map(({ x, y }) => ({ x, y }))).toEqual(expect.arrayContaining(mapMatcher));
+    expect(bomReturn).toBe(bomCount);
+  });
+});
