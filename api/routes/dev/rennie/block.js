a5e2ff631a09321cdb0e0c9371b26634d747d203
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-10 07:50:03 +0900

    12/10重複テスト

--- api/routes/dev/rennie/block.js
+++ api/routes/dev/rennie/block.js
@@ -38,33 +38,4 @@ router.route('/').delete((req, res) => {
   res.json(field);
 });
 
-// const field = [{ x: 0, y: 0 }];
-
-// router.route('/').post((req, res) => {
-//   if (req.body.x) {
-//     const directions = [[0, -1], [1, 0], [0, 1], [-1, 0], [-1, -1], [1, 1], [-1, 1], [1, -1]];
-//     for (i = 0; i < directions.length; i++) {
-//       const a = Number(directions[i][0]);
-//       const b = Number(directions[i][1]);
-//       let m = field.length;
-//       while (m >= 0) {
-//         if (field[m - 1].y + b === +req.body.y && field[m - 1].x + a === +req.body.x) {
-//           field.push({ x: +req.body.x, y: +req.body.y });
-//           m -= 1;
-//           break;
-//         }
-//         m -= 1;
-//         break;
-//       }
-//     }
-//   }
-//   console.log(field);
-//   res.json(field);
-// });
-
-// router.route('/').delete((req, res) => {
-//   field.length = 1;
-//   res.json(field);
-// });
-
 module.exports = router;
--- api/tests/routes/dev/rennie/block.test.js
+++ api/tests/routes/dev/rennie/block.test.js
@@ -30,27 +30,4 @@ describe('前のゲーム情報のリセット処理、および、リクエス�
     expect(lastBody).toHaveLength(positions2.length + 1);
     expect(lastBody).toEqual(expect.arrayContaining([initialBlock(), ...positions2]));
   });
-  // it('周囲8方向を開くことができる', async () => {
-  //   // 前のテストのBlockをサーバーから消しておく
-  //   await chai.request(app).delete('/dev/rennie/block');
-  //   // Given
-  //   const positions = [{ x: 1, y: 0 }, { x: 2, y: 0 }, { x: 5, y: 0 }];
-  //   // When
-  //   let lastBody;
-  //   for (let i = 0; i < positions.length; i += 1) {
-  //     const { body } = await chai
-  //       .request(app)
-  //       .post('/dev/rennie/block')
-  //       .set('content-type', 'application/x-www-form-urlencoded')
-  //       .send(positions[i]);
-  //     lastBody = body;
-  //   }
-  //   // Then
-  //   // 8方向にいく
-
-  //   const matchers = [{ x: 1, y: 0 }, { x: 2, y: 0 }];
-
-  //   expect(lastBody).toHaveLength(matchers.length + 1);
-  //   expect(lastBody).toEqual(expect.arrayContaining([initialBlock(), ...matchers]));
-  // });
 });
