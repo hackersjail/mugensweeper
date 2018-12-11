7b3ee67cf91a2904fe58cb7dd9074ef45a96aead
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-12-12 07:38:52 +0900

    コード修正しました

--- api/models/miyamoto/bomMap.js
+++ api/models/miyamoto/bomMap.js
@@ -1,27 +1,35 @@
 const initSet = function(bomCount, position) {
   const directions = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
-  let counter = 0;
+  let flag = true;
+  let cnt = 0;
   const positions = [];
+  const tmp = [];
   let tmpX;
   let tmpY;
+
+  while (tmp.length < bomCount) {
+    const value = Math.floor(Math.random() * 8);
+    flag = true;
+    for (let i = 0; i < tmp.length; i += 1) {
+      if (tmp[i] === value) flag = false;
+    }
+    if (flag) tmp.push(value);
+  }
+
+  const tmp2 = tmp.sort((a, b) => a - b);
 
   for (let i = 0; i < directions.length; i += 1) {
     tmpX = position.x - directions[i][0];
     tmpY = position.y - directions[i][1];
-    if (bomCount - counter >= directions.length - i) {
+
+    if (tmp2[cnt] === i && cnt < bomCount) {
       positions.push({ x: tmpX, y: tmpY, bom: 1 });
-      counter += 1;
-    } else if (Math.floor(Math.random() * 10) % 3 === 0) {
-      if (counter < bomCount) {
-        positions.push({ x: tmpX, y: tmpY, bom: 1 });
-        counter += 1;
-      } else {
-        positions.push({ x: tmpX, y: tmpY, bom: 0 });
-      }
+      cnt += 1;
     } else {
       positions.push({ x: tmpX, y: tmpY, bom: 0 });
     }
   }
+
   return positions;
 };
 
