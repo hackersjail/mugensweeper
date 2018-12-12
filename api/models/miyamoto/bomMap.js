5a7937419b77c2d8453e520ace8bf6fd9ce3b5ca
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-12-13 05:57:15 +0900

    ダサいソースを修正しました

--- api/models/miyamoto/bomMap.js
+++ api/models/miyamoto/bomMap.js
@@ -1,34 +1,28 @@
 const initSet = function(bomCount, position) {
   const directions = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
-  let flag = true;
-  let cnt = 0;
   const positions = [];
   const tmp = [];
-  let tmpX;
-  let tmpY;
 
   while (tmp.length < bomCount) {
     const value = Math.floor(Math.random() * 8);
-    flag = true;
-    for (let i = 0; i < tmp.length; i += 1) {
-      if (tmp[i] === value) flag = false;
-    }
-    if (flag) tmp.push(value);
+    if (!tmp.includes(value)) tmp.push(value);
   }
 
-  const tmp2 = tmp.sort((a, b) => a - b);
-
-  for (let i = 0; i < directions.length; i += 1) {
-    tmpX = position.x - directions[i][0];
-    tmpY = position.y - directions[i][1];
-
-    if (tmp2[cnt] === i && cnt < bomCount) {
-      positions.push({ x: tmpX, y: tmpY, bom: 1 });
-      cnt += 1;
-    } else {
-      positions.push({ x: tmpX, y: tmpY, bom: 0 });
-    }
-  }
+  [...Array(directions.length)].reduce(
+    (acc, c, idx) =>
+      tmp.includes(idx)
+        ? positions.push({
+            x: position.x - directions[idx][0],
+            y: position.y - directions[idx][1],
+            bom: 1,
+          })
+        : positions.push({
+            x: position.x - directions[idx][0],
+            y: position.y - directions[idx][1],
+            bom: 0,
+          }),
+    '',
+  );
 
   return positions;
 };
