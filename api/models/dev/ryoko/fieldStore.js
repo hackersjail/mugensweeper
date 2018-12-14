4b2c61713d4f10c07899801798c2b7ca956eed37
lilycoco <ry.sep27@gmail.com>
2018-12-15 07:27:39 +0900

    added mongodb_test

---
+++ api/models/dev/ryoko/fieldStore.js
@@ -0,0 +1,16 @@
+const FieldModel = require('./FieldModel.js');
+
+const propFilter = '-_id -__v';
+
+module.exports = {
+  async initField() {
+    await FieldModel.remove();
+    await new FieldModel({ x: 0, y: 0 }).save();
+  },
+  getField() {
+    return FieldModel.find({}, propFilter).lean();
+  },
+  async addBlock(block) {
+    await new FieldModel(block).save();
+  },
+};
