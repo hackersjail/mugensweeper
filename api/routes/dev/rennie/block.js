4c03e5fb4aa631dd43c959aba2014e72ebdc31bc
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-16 07:14:21 +0900

    12/16 mongoose まで

--- api/routes/dev/rennie/block.js
+++ api/routes/dev/rennie/block.js
@@ -3,67 +3,59 @@ const router = require('express').Router();
 const { getField, initField, addBlock } = require('../../../models/dev/rennie/fieldStore.js');
 // const field = await getField();
 
-router.route('/').post(async (req, res) => {
-  if (req.body.x) {
-    // ここはmonngooseで追加したとこ（この３行）
-    // const block = { x: +req.body.x, y: +req.body.y };
-    await addBlock({ x: +req.body.x, y: +req.body.y });
-    // await new FieldModel(block).save();
-  }
-  res.json(await getField());
-
-  // console.log(await)
-});
-
-router.route('/').delete(async (req, res) => {
-  await initField();
-  // field.length = 1;
-  res.json(await getField());
-});
-
-// // 同じ座標にはpostしても登録されない
-// const field = [{ x: 0, y: 0 }];
-
-// router.route('/').post((req, res) => {
+// router.route('/').post(async (req, res) => {
 //   if (req.body.x) {
-//     let m = field.length;
-
-//     while (m >= 0) {
-//       if (field[m - 1].x !== +req.body.x) {
-//         if (m - 1 === 0) {
-//           field.push({ x: +req.body.x, y: +req.body.y });
-//           m -= 1;
-//           break;
-//         }
-//         m -= 1;
-//       }
-//       if (field[m - 1].y !== +req.body.y && field[m - 1].x === +req.body.x) {
-//         m -= 1;
-//         if (m - 1 === 0) {
-//           field.push({ x: +req.body.x, y: +req.body.y });
-//           m -= 1;
-//           break;
-//         }
-//       }
-//       if (field[m - 1].x === +req.body.x && field[m - 1].y === +req.body.y) {
-//         break;
-//       }
-//       field.push({ x: +req.body.x, y: +req.body.y });
-//       break;
-//     }
+//     // ここはmonngooseで追加したとこ（この３行）
+//     // const block = { x: +req.body.x, y: +req.body.y };
+//     await addBlock({ x: +req.body.x, y: +req.body.y });
+//     // await new FieldModel(block).save();
 //   }
-//   res.json(field);
+//   res.json(await getField());
+
+//   // console.log(await)
 // });
 
-// router.route('/').delete((req, res) => {
-//   field.length = 1;
-//   res.json(field);
-// });
 // router.route('/').delete(async (req, res) => {
 //   await initField();
 //   // field.length = 1;
 //   res.json(await getField());
 // });
+
+// // 同じ座標にはpostしても登録されない
+// // const field = [{ x: 0, y: 0 }];
+
+// router.route('/').delete(async (req, res) => {
+//   await initField();
+//   // field.length = 1;
+//   res.json(await getField());
+// });//
+
+// router.route('/').delete(async (req, res) => {
+//   await initField();
+//   // field.length = 1;
+//   res.json(await getField());
+// });//
+
+// router.route('/').post(async(req, res) => {
+//   if (req.body.x) {
+//     const field = await getField();
+//     const a = +req.body.x;
+//     const b = +req.body.y;
+//     const dup = field.findIndex((e) => a === e.x && b === e.y);
+
+//     if (dup === -1) {
+//       await addBlock({ x :a , y: b });
+//     }
+
+//     res.json(await getField());
+//   }
+// });
+
+//  router.route('/').delete(async (req, res) => {
+//   await initField();
+//   // field.length = 1;
+//   res.json(await getField());
+// });//
 
 // ８方向に開く
 router.route('/').post(async (req, res) => {
@@ -74,7 +66,6 @@ router.route('/').post(async (req, res) => {
     const field = await getField();
     let match = true;
     let count = 0;
-    // console.log('field', field);
 
     for (let m = 0; m < field.length; m += 1) {
       for (let i = 0; i < directions.length; i += 1) {
--- api/tests/routes/dev/rennie/block.test.js
+++ api/tests/routes/dev/rennie/block.test.js
@@ -8,84 +8,81 @@ const propFilter = '-_id -__v';
 
 const initialBlock = () => ({ x: 0, y: 0 });
 
-describe('前のゲーム情報のリセット処理、および、リクエスト返り値の追加テスト', () => {
-  beforeAll(connectDB);
-  beforeEach(initField);
-  afterEach(dropDB);
-  afterAll(disconnectDB);
-  it('座標をリセットできる。', async () => {
-    // Given
-    const positions = [{ x: 1, y: 2 }, { x: 3, y: 1 }];
-    // When
-    let lastBody;
-    for (let i = 0; i < positions.length; i += 1) {
-      const { body } = await chai
-        .request(app)
-        .post('/dev/rennie/block')
-        .set('content-type', 'application/x-www-form-urlencoded')
-        .send(positions[i]);
-      lastBody = body;
-    }
+// describe('前のゲーム情報のリセット処理、および、リクエスト返り値の追加テスト', () => {
+//   beforeAll(connectDB);
+//   beforeEach(initField);
+//   afterEach(dropDB);
+//   afterAll(disconnectDB);
+//   it('座標をリセットできる。', async () => {
+//     // Given
+//     const positions = [{ x: 1, y: 2 }, { x: 3, y: 1 }];
+//     // When
+//     let lastBody;
+//     for (let i = 0; i < positions.length; i += 1) {
+//       const { body } = await chai
+//         .request(app)
+//         .post('/dev/rennie/block')
+//         .set('content-type', 'application/x-www-form-urlencoded')
+//         .send(positions[i]);
+//       lastBody = body;
+//     }
 
-    const beforeDeleteField = await FieldModel.find({}, propFilter).lean();
-    const { body } = await chai.request(app).delete('/dev/rennie/block');
-    const afterDeleteField = await FieldModel.find({}, propFilter).lean();
+//     const beforeDeleteField = await FieldModel.find({}, propFilter).lean();
+//     const { body } = await chai.request(app).delete('/dev/rennie/block');
+//     const afterDeleteField = await FieldModel.find({}, propFilter).lean();
 
-    // Then
-    expect(lastBody).toHaveLength(positions.length + 1);
-    expect(lastBody).toEqual(expect.arrayContaining([initialBlock(), ...positions]));
-    expect(body).toEqual(expect.arrayContaining([initialBlock()]));
+//     // Then
+//     expect(lastBody).toHaveLength(positions.length + 1);
+//     expect(lastBody).toEqual(expect.arrayContaining([initialBlock(), ...positions]));
+//     expect(body).toEqual(expect.arrayContaining([initialBlock()]));
 
-    // 4. Then:db
-    expect(beforeDeleteField).toHaveLength(positions.length + 1);
-    expect(beforeDeleteField).toEqual(expect.arrayContaining([initialBlock(), ...positions]));
-    expect(afterDeleteField).toEqual(expect.arrayContaining([initialBlock()]));
-  });
-});
-
-//  beforeAll(connectDB);
-//  beforeEach(initField);
-//  afterEach(dropDB);
-//  afterAll(disconnectDB);
+//     // 4. Then:db
+//     expect(beforeDeleteField).toHaveLength(positions.length + 1);
+//     expect(beforeDeleteField).toEqual(expect.arrayContaining([initialBlock(), ...positions]));
+//     expect(afterDeleteField).toEqual(expect.arrayContaining([initialBlock()]));
+//   });
+// });
 
 // describe('field apiについてのテスト', () => {
-// beforeAll(connectDB);
-// beforeEach(initField);
-// afterEach(dropDB);
-// afterAll(disconnectDB);
+//   beforeAll(connectDB);
+//   beforeEach(initField);
+//   afterEach(dropDB);
+//   afterAll(disconnectDB);
 
-//  it('同じ座標にはpostしても登録されない', async () => {
-//   // 前のテストのBlockをサーバーから消しておく
-//   await chai.request(app).delete('/dev/rennie/block');
-//   // Given
-//   const positions = [{ x: 1, y: 0 }, { x: 1, y: 0 }];
-//   // When
-//   let lastBody;
-//   for (let i = 0; i < positions.length; i += 1) {
-//     const { body } = await chai
-//       .request(app)
-//       .post('/dev/rennie/block')
-//       .set('content-type', 'application/x-www-form-urlencoded')
-//       .send(positions[i]);
-//     lastBody = body;
-//   }
+//   it('同じ座標にはpostしても登録されない', async () => {
+//     // 前のテストのBlockをサーバーから消しておく
+//     await chai.request(app).delete('/dev/rennie/block');
+//     // Given
+//     const positions = [{ x: 1, y: 0 }, { x: 1, y: 0 }];
+//     // When
+//     let lastBody;
+//     for (let i = 0; i < positions.length; i += 1) {
+//       const { body } = await chai
+//         .request(app)
+//         .post('/dev/rennie/block')
+//         .set('content-type', 'application/x-www-form-urlencoded')
+//         .send(positions[i]);
+//       lastBody = body;
+//     }
 
-//   // const beforeDeleteField = await FieldModel.find({}, propFilter).lean();
-//   const { body } = await chai.request(app).post('/dev/rennie/block');
-//   const afterField = await FieldModel.find({}, propFilter).lean();
+//     // const beforeDeleteField = await FieldModel.find({}, propFilter).lean();
+//     const { body } = await chai.request(app).post('/dev/rennie/block');
+//     const afterField = await FieldModel.find({}, propFilter).lean();
+//     console.log(body);
 
-//   // Then
-//   // 重複削除
-//   const positions2 = positions.filter(
-//     (v1, i1, a1) => a1.findIndex((v2) => v1.x === v2.x && v1.y === v2.y) === i1,
-//   );
-//   expect(lastBody).toHaveLength(positions2.length + 1);
-//   expect(lastBody).toEqual(expect.arrayContaining([initialBlock(), ...positions2]));
+//     // Then
+//     // 重複削除
+//     const positions2 = positions.filter(
+//       (v1, i1, a1) => a1.findIndex((v2) => v1.x === v2.x && v1.y === v2.y) === i1,
+//     );
+//     expect(lastBody).toHaveLength(positions2.length + 1);
+//     expect(lastBody).toEqual(expect.arrayContaining([initialBlock(), ...positions2]));
 
-// //Then2
-//   expect(afterField).toHaveLength(positions2.length + 1);
-//   expect(afterField).toEqual(expect.arrayContaining([initialBlock(), ...positions]));
-//   // expect(afterDeleteField).toEqual(expect.arrayContaining([initialBlock()]));
+//     // Then2
+//       expect(afterField).toHaveLength(positions2.length + 1);
+//       expect(afterField).toEqual(expect.arrayContaining([initialBlock(), ...positions]));
+//       // expect(afterDeleteField).toEqual(expect.arrayContaining([initialBlock()]));
+//   });
 // });
 
 describe('field apiについてのテスト', () => {
@@ -121,6 +118,5 @@ describe('field apiについてのテスト', () => {
     // Then2
     expect(afterField).toHaveLength(matchers.length + 1);
     expect(afterField).toEqual(expect.arrayContaining([initialBlock(), ...matchers]));
-    // expect(afterDeleteField).toEqual(expect.arrayContaining([initialBlock()]));
   });
 });
