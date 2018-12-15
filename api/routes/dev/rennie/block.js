6317a3cd4b5d8a077a2d7e3df31a82573bbc518e
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-16 07:26:18 +0900

    12/16 mongooseまでno2

--- api/routes/dev/rennie/block.js
+++ api/routes/dev/rennie/block.js
@@ -1,61 +1,5 @@
 const router = require('express').Router();
-// const FieldModel = require('../../../models/dev/rennie/fieldStore.js');
 const { getField, initField, addBlock } = require('../../../models/dev/rennie/fieldStore.js');
-// const field = await getField();
-
-// router.route('/').post(async (req, res) => {
-//   if (req.body.x) {
-//     // ここはmonngooseで追加したとこ（この３行）
-//     // const block = { x: +req.body.x, y: +req.body.y };
-//     await addBlock({ x: +req.body.x, y: +req.body.y });
-//     // await new FieldModel(block).save();
-//   }
-//   res.json(await getField());
-
-//   // console.log(await)
-// });
-
-// router.route('/').delete(async (req, res) => {
-//   await initField();
-//   // field.length = 1;
-//   res.json(await getField());
-// });
-
-// // 同じ座標にはpostしても登録されない
-// // const field = [{ x: 0, y: 0 }];
-
-// router.route('/').delete(async (req, res) => {
-//   await initField();
-//   // field.length = 1;
-//   res.json(await getField());
-// });//
-
-// router.route('/').delete(async (req, res) => {
-//   await initField();
-//   // field.length = 1;
-//   res.json(await getField());
-// });//
-
-// router.route('/').post(async(req, res) => {
-//   if (req.body.x) {
-//     const field = await getField();
-//     const a = +req.body.x;
-//     const b = +req.body.y;
-//     const dup = field.findIndex((e) => a === e.x && b === e.y);
-
-//     if (dup === -1) {
-//       await addBlock({ x :a , y: b });
-//     }
-
-//     res.json(await getField());
-//   }
-// });
-
-//  router.route('/').delete(async (req, res) => {
-//   await initField();
-//   // field.length = 1;
-//   res.json(await getField());
-// });//
 
 // ８方向に開く
 router.route('/').post(async (req, res) => {
@@ -82,15 +26,12 @@ router.route('/').post(async (req, res) => {
 
     if (count !== 0 && match === true) {
       await addBlock({ x, y });
-      // field.push({ x, y });
-      // await new FieldModel(block).save();
     }
     res.json(await getField());
   }
 });
 
 router.route('/').delete(async (req, res) => {
-  // field.length = 1;
   await initField();
   res.json(await getField());
 });
