148ffc33c00ae9c89af5321ea3ba5abfc7d43e9f
lilycoco <ry.sep27@gmail.com>
2018-12-16 18:06:50 +0900

    added RyokoFieldModel

--- api/routes/dev/ryoko/block.js
+++ api/routes/dev/ryoko/block.js
@@ -1,38 +1,38 @@
 const router = require('express').Router();
 const { getField, initField, addBlock } = require('../../../models/dev/ryoko/fieldStore.js');
 
-router.route('/').post(async (req, res) => {
-  // async: promiseを返す関数に関数を変化させる
+const directions = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
 
-  // 開いた場所の周囲
-  const directions = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
-  const field = await getField();
+router
+  .route('/')
+  .post(async (req, res) => {
+    // async: promiseを返す関数に関数を変化させる
 
-  if (req.body.x) {
-    for (let i = 0; i < field.length; i += 1) {
-      for (let t = 0; t < directions.length; t += 1) {
-        const a = directions[t][0];
-        const b = directions[t][1];
-        const u = field[i].x + a; // 隣接するx座標
-        const k = field[i].y + b; // 隣接するy座標
+    // 開いた場所の周囲
+    const field = await getField();
 
-        if (field[i].x === Number(req.body.x) && field[i].y === Number(req.body.y)) {
-          break;
-        } else if (
-          i === field.length - 1 &&
-          (u === Number(req.body.x) && k === Number(req.body.y))
-        ) {
-          await addBlock({ x: Number(req.body.x), y: Number(req.body.y) });
+    if (req.body.x) {
+      for (let i = 0; i < field.length; i += 1) {
+        for (let t = 0; t < directions.length; t += 1) {
+          const u = field[i].x + directions[t][0]; // 隣接するx座標
+          const k = field[i].y + directions[t][1]; // 隣接するy座標
+
+          if (field[i].x === Number(req.body.x) && field[i].y === Number(req.body.y)) {
+            break;
+          } else if (
+            i === field.length - 1 &&
+            (u === Number(req.body.x) && k === Number(req.body.y))
+          ) {
+            await addBlock({ x: Number(req.body.x), y: Number(req.body.y) });
+          }
         }
       }
     }
-  }
-  res.json(await getField());
-});
-
-router.route('/').delete(async (req, res) => {
-  await initField();
-  res.json(await getField());
-});
+    res.json(await getField());
+  })
+  .delete(async (req, res) => {
+    await initField();
+    res.json(await getField());
+  });
 
 module.exports = router;
--- api/tests/routes/dev/ryoko/field.test.js
+++ api/tests/routes/dev/ryoko/field.test.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const app = require('../../../../routes/app.js');
 const { connectDB, disconnectDB, dropDB } = require('../../../../database.js');
+const RyokoFieldModel = require('../../../../models/dev/ryoko/FieldModel.js');
 const { initField } = require('../../../../models/dev/ryoko/fieldStore.js');
 
-const initialBlock = () => ({
-  x: 0,
-  y: 0,
-});
+const propFilter = '-_id -__v';
+
+const initialBlock = () => ({ x: 0, y: 0 });
 
 describe('field app についてのテスト', () => {
   beforeAll(connectDB); // 全てのitの前
@@ -20,9 +20,14 @@ describe('field app についてのテスト', () => {
 
     // When
     const { body } = await chai.request(app).get(`/dev/ryoko/field`);
+    const initialField = await RyokoFieldModel.find({}, propFilter).lean();
 
     // 3: Then
+    // Response
     expect(body).toHaveLength(1);
     expect(body[0]).toMatchObject(initialBlock());
+    // DB
+    expect(initialField).toHaveLength(1);
+    expect(initialField[0]).toMatchObject(initialBlock());
   });
 });
