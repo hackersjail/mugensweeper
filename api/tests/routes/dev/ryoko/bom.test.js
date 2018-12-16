354cb8bef59ea0605595d558a783bfe472806fb0
lilycoco <ry.sep27@gmail.com>
2018-12-17 08:46:03 +0900

    added test case:ユーザー名文字数制限

--- api/tests/routes/dev/ryoko/bom.test.js
+++ api/tests/routes/dev/ryoko/bom.test.js
@@ -21,29 +21,4 @@ describe('爆弾マップのテスト', () => {
     );
     expect(bomReturn).toBe(bomCount);
   });
-
-  // it('原点の周囲の爆弾マップと爆弾を置ける余剰数が返ってくる', () => {
-  //   // Given
-  //   const bomCount = Math.ceil(Math.random() * 3);
-  //   const initBlock = { x: 0, y: 0 };
-  //   const position = { x: 1, y: 0 };
-
-  //   // When
-  //   const map = bomMap.initSet(bomCount, position);
-  //   // [{x,y,bom: t|f},...]
-
-  //   // Then
-  //   const mapMatcher = directions.map(([x, y]) => ({ x, y }));
-  //   const bomReturn = map.filter(({ bom }) => bom).length;
-  //   const spaceReturn = map.filter(({ leftSpace }) => leftSpace).length;
-
-  //   expect(map).toHaveLength(8);
-  //   expect(spaceReturn).toBe(8);
-  //   expect(bomReturn).toBe(bomCount);
-
-  //   expect(map.map(({ x, y }) => ({ x, y }))).toMatchObject(
-  //     expect.arrayContaining([...mapMatcher]),
-
-  //   );
-  // });
 });
