a223408853dfad5b4cb25f1c2bf655e82fa9085a
Takuto Eto <aoitele@gmail.com>
2018-12-22 02:55:52 +0900

    block.jsを修正

--- api/routes/dev/eto/block.js
+++ api/routes/dev/eto/block.js
@@ -4,54 +4,55 @@ const { getField, initField, addBlock } = require('../../../models/dev/eto/field
 // prettier-ignore
 const directions =[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]];
 
-// 周囲8マス確認用の配列
-let matchers = [
-  { x: -1, y: -1 },
-  { x: -1, y: 0 },
-  { x: -1, y: 1 },
-  { x: 0, y: -1 },
-  { x: 0, y: 1 },
-  { x: 1, y: -1 },
-  { x: 1, y: 0 },
-  { x: 1, y: 1 },
-];
+let positions = [{ x: 0, y: 0 }];
+let around = [];
+let dubliCheck = false;
 
 // POSTした座標を追加する処理
 router.route('/').post(async (req, res) => {
   // 送信された配列
   const block = {
-    x: Number(req.body.x),
-    y: Number(req.body.y),
+    x: +req.body.x,
+    y: +req.body.y,
   };
 
-  // 周囲8マス以外はrejectする
-  if (matchers.length > 0) {
-    // 最新POSTの周囲8マスと合致するかチェック;
-    for (let i = 0; i < matchers.length; i += 1) {
-      if (matchers[i].x === block.x && matchers[i].y === block.y) {
+  // 重複チェック
+  for (let i = 0; i < positions.length; i += 1) {
+    if (block.x === positions[i].x && block.y === positions[i].y) {
+      dubliCheck = true;
+    }
+  }
+
+  if (dubliCheck === false) {
+    // 配列の周囲8マス(around)を生成
+    for (let i = 0; i < positions.length; i += 1) {
+      for (let k = 0; k < directions.length; k += 1) {
+        const aroundX = positions[i].x + directions[k][0];
+        const aroundY = positions[i].y + directions[k][1];
+        around.push({ x: aroundX, y: aroundY });
+      }
+    }
+    // aroundの重複を削除
+    const around2 = around.filter(
+      (v1, i1, a1) => a1.findIndex((v2) => v1.x === v2.x && v1.y === v2.y) === i1,
+    );
+
+    // blockが配列の周囲8マス(around2)に含まれるかチェック
+    for (let i = 0; i < around2.length; i += 1) {
+      if (block.x === around2[i].x && block.y === around2[i].y) {
+        positions.push(block);
         await addBlock(block);
       }
     }
   }
-  // 周囲8マス配列を更新
-  matchers = [];
-  for (let i = 0; i < directions.length; i += 1) {
-    const aroundX = block.x + directions[i][0];
-    const aroundY = block.y + directions[i][1];
-    matchers.push({
-      x: aroundX,
-      y: aroundY,
-    });
-  }
+  dubliCheck = false;
+  around = [];
   res.send(await getField());
 });
 
 // 配列を初期化する処理
 router.route('/').delete(async (req, res) => {
-  // 周囲8マス配列を更新
-  // prettier-ignore
-  matchers = [{ x: -1, y: -1 },{ x: -1, y: 0 },{ x: -1, y: 1 },{ x: 0, y: -1 },{ x: 0, y: 1 },{ x: 1, y: -1 },{ x: 1, y: 0 },
-  { x: 1, y: 1 },];
+  positions = [{ x: 0, y: 0 }];
   await initField();
   res.send(await getField());
 });
