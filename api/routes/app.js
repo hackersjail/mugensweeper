5ff1785b77d3e97e75aab95bcad6e7c8ca14fce9
lilycoco <ry.sep27@gmail.com>
2018-12-21 22:28:39 +0900

    t

--- api/routes/app.js
+++ api/routes/app.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/dev', require('./dev/index.js'));
-app.use('/v1/userId', require('./v1/userId.js'));
+app.use('/v1', require('./v1/index.js'));
 
 module.exports = app;
--- api/tests/routes/v1/user/user.test.js
+++ api/tests/routes/v1/user/user.test.js
@@ -1,7 +1,23 @@
 const chai = require('chai');
-const app = require('../../../routes/app.js');
+const app = require('../../../../routes/app.js');
 
-describe('ユーザー名・ID登録', () => {
+describe('ユーザー情報を返せるかどうか', () => {
+  it('任意で複数ユーザー名をpostして、ユーザーIDつきでリーターンできるか', async () => {
+    // Given
+    const userName = ['yui', 'taro'];
+    // When
+    for (let i = 0; i < userName.length; i += 1) {
+      const { body } = await chai
+        .request(app)
+        .post('/v1/user')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({ userName: userName[i] });
+      // then
+      expect(body[i]).toHaveProperty('userID');
+      expect(body[i]).toHaveProperty('userName');
+      expect(body[i].userName).toBe(userName[i]);
+    }
+  });
   it('ユーザー名の文字数(3~7文字)、文字種(*,/,￥,＆,")制限', async () => {
     // 1: Given
     const nameList = [
@@ -17,7 +33,7 @@ describe('ユーザー名・ID登録', () => {
       const { name } = nameList[i];
       const { status } = await chai
         .request(app)
-        .post('/v1/userId')
+        .post('/v1/user_name')
         .set('content-type', 'application/x-www-form-urlencoded')
         .send({ name });
       results.push(status);
