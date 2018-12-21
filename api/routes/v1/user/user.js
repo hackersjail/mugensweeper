769db8763efb17f76398eb191ff0bf38af84c62e
lilycoco <ry.sep27@gmail.com>
2018-12-22 07:06:41 +0900

    key を揃えました

--- api/routes/v1/user/user.js
+++ api/routes/v1/user/user.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const user = [];
 router.route('/').post((req, res) => {
-  if (req.body.name === 'yui' || req.body.name === 'taro') {
-    const block = { name: req.body.name, userID: 222222 };
+  if (req.body.userName === 'yui' || req.body.userName === 'taro') {
+    const block = { userName: req.body.userName, userId: 222222 };
     user.push(block);
     res.json(user);
-  } else if (req.body.name === 'tttt') {
+  } else if (req.body.userName === 'tttt') {
     res.status(200).send('登録完了');
   } else {
     res.status(401).send('error');
--- api/tests/routes/v1/user/user.test.js
+++ api/tests/routes/v1/user/user.test.js
@@ -4,39 +4,39 @@ const app = require('../../../../routes/app.js');
 describe('ユーザー情報を返せるかどうか', () => {
   it('任意で複数ユーザー名をpostして、ユーザーIDつきでリーターンできるか', async () => {
     // Given
-    const nameList = [{ name: 'yui' }, { name: 'taro' }];
+    const nameList = [{ userName: 'yui' }, { userName: 'taro' }];
     // When
     for (let i = 0; i < nameList.length; i += 1) {
-      const { name } = nameList[i];
+      const { userName } = nameList[i];
       const { body } = await chai
         .request(app)
         .post('/v1/user')
         .set('content-type', 'application/x-www-form-urlencoded')
-        .send({ name });
+        .send({ userName });
       // then
-      expect(body[i]).toHaveProperty('userID');
-      expect(body[i]).toHaveProperty('name');
-      expect(body[i].name).toBe(name);
+      expect(body[i]).toHaveProperty('userId');
+      expect(body[i]).toHaveProperty('userName');
+      expect(body[i].userName).toBe(userName);
     }
   });
   it('ユーザー名の文字数(3~7文字)、文字種(*,/,￥,＆,")制限', async () => {
     // 1: Given
     const nameList = [
-      { name: 'ss', judge: false },
-      { name: 'tttt', judge: true },
-      { name: 'jjjjjjjjjjjj', judge: false },
-      { name: '#$%w', judge: false },
+      { userName: 'ss', judge: false },
+      { userName: 'tttt', judge: true },
+      { userName: 'jjjjjjjjjjjj', judge: false },
+      { userName: '#$%w', judge: false },
     ];
 
     // 2: When
     const results = [];
     for (let i = 0; i < nameList.length; i += 1) {
-      const { name } = nameList[i];
+      const { userName } = nameList[i];
       const { status } = await chai
         .request(app)
         .post('/v1/user')
         .set('content-type', 'application/x-www-form-urlencoded')
-        .send({ name });
+        .send({ userName });
       results.push(status);
     }
 
