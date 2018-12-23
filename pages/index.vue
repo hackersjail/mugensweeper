ca34060cf74890be2cdb6ad3048d1023ec3b6e3d
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-12-24 06:29:50 +0900

    prettierによって自動的に修正されたvueファイルのcommit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
+      <Logo />
       <h1 class="title">
         mugensweeper
       </h1>
@@ -12,11 +12,17 @@
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green">Documentation</a>
+          class="button--green"
+        >
+          Documentation
+        </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey"
+        >
+          GitHub
+        </a>
       </div>
     </div>
   </section>
