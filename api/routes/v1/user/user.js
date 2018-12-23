73ffc33f763a5e535c86797cf40547fb649e4510
lilycoco <ry.sep27@gmail.com>
2018-12-23 16:06:56 +0900

    ランダムなID追加

--- api/routes/v1/user/user.js
+++ api/routes/v1/user/user.js
@@ -5,14 +5,21 @@ router.route('/').post((req, res) => {
   const { length } = req.body.userName;
   const { userName } = req.body;
 
+  // ユニークな8桁のIDを生成
+  const c = 'abcdefghijklmnopqrstuvwxyz0123456789'; // 生成する文字列に含める文字
+  let r = '';
+  for (let i = 0; i < 8; i += 1) {
+    r += c[Math.floor(Math.random() * c.length)];
+  }
+
+  // 半角文字のみ許可
   const harfLetter = (value) => !value.match(/[^\x01-\x7E]/) || !value.match(/[^\uFF65-\uFF9F]/); // eslint-disable-line
 
   if (length > 2 && length < 8 && harfLetter(userName)) {
-    user.push({ userName, userId: 222222 });
-    res.json(user);
-    res.status(200).send('success');
+    user.push({ userName, userId: r });
+    res.status(200).send(user);
   } else {
-    res.status(401).send('error');
+    res.status(401).send('Error');
   }
 });
 
