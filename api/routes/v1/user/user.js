4cb532a184e249ae637f8779c350f46288b73199
lilycoco <ry.sep27@gmail.com>
2018-12-24 10:41:31 +0900

    IDがユニークかどうか確認

--- api/routes/v1/user/user.js
+++ api/routes/v1/user/user.js
@@ -1,22 +1,34 @@
 const router = require('express').Router();
 
 const user = [];
+
 router.route('/').post((req, res) => {
   const { length } = req.body.userName;
   const { userName } = req.body;
 
   // ユニークな8桁のIDを生成
   const c = 'abcdefghijklmnopqrstuvwxyz0123456789'; // 生成する文字列に含める文字
-  let r = '';
-  for (let i = 0; i < 8; i += 1) {
-    r += c[Math.floor(Math.random() * c.length)];
-  }
+  const id = [];
+  while (id.length < 1) {
+    let r = '';
+    for (let i = 0; i < 8; i += 1) {
+      r += c[Math.floor(Math.random() * c.length)];
+    }
 
+    if (user.length < 1) {
+      id.push(r);
+    } else {
+      const find = user.find((v) => v.userId === r);
+      if (!find) {
+        id.push(r);
+      }
+    }
+  }
   // 半角文字のみ許可
   const harfLetter = (value) => !value.match(/[^\x01-\x7E]/) || !value.match(/[^\uFF65-\uFF9F]/); // eslint-disable-line
 
-  if (length > 2 && length < 8 && harfLetter(userName)) {
-    user.push({ userName, userId: r });
+  if (length >= 3 && length <= 7 && harfLetter(userName)) {
+    user.push({ userName, userId: id[0] });
     res.status(200).send(user);
   } else {
     res.status(401).send('Error');
