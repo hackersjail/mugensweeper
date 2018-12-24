71cae53eaaac97a790fa86019d87372441075d45
hi85 <33993836+hi85gh@users.noreply.github.com>
2018-12-24 20:31:43 +0900

    Update API server port number

--- api/config.js
+++ api/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 10000,
+  PORT: process.env.API_PORT || 10000,
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/mugensweeper',
 };
