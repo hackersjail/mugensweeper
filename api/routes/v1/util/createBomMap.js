7137b751871b235cc783615bda6ffd0dd8dd6802
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-27 10:39:30 +0900

    12/27 bomMap作成 no8

--- api/routes/v1/util/createBomMap.js
+++ api/routes/v1/util/createBomMap.js
@@ -1,8 +1,1 @@
-module.exports = (arr) => {
-  const newBomMap = [];
-  for (let i = 0; i < arr.length; i += 1) {
-    const block = { x: arr[i].x, y: arr[i].y };
-    newBomMap.push(block);
-  }
-  return newBomMap;
-};
+module.exports = (arr) => arr.map(({ x, y }) => ({ x, y }));
