520992c7d572737019f5225f4cd7d48f1340c4c1
lilycoco <ry.sep27@gmail.com>
2019-01-05 01:28:02 +0900

    added response test to field.test.js

---
+++ api/routes/v1/field/field.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { getData, addData } = require('../../../models/v1/fieldStore.js');
+
+router.route('/').post(async (req, res) => {
+  await addData({ x: +req.body.x, y: +req.body.y });
+  res.json(await getData());
+});
+
+module.exports = router;
