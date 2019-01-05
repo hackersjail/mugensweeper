e610be5487331deeb794cf090814e2797b0cf780
NaomiRennie <naomi.lily.405@gmail.com>
2019-01-05 13:01:22 +0900

    修正しましたno5

--- api/routes/v1/util/judgeExploded.js
+++ api/routes/v1/util/judgeExploded.js
@@ -1,6 +1,5 @@
 module.exports = (block, bom) => {
   const match = bom.find((bomBlock) => bomBlock.x === block.x && bomBlock.y === block.y);
-
-  const a = match !== undefined ? { ...block, exploded: true } : { ...block, exploded: false };
-  return a;
+  const exploded = match !== undefined;
+  return { ...block, exploded };
 };
