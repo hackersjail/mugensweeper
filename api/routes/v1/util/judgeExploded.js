25e22d304eb488e8904aa8a814e581b246c7785b
NaomiRennie <naomi.lily.405@gmail.com>
2019-01-05 10:53:29 +0900

    修正しましたno2

--- api/routes/v1/util/judgeExploded.js
+++ api/routes/v1/util/judgeExploded.js
@@ -1,14 +1,7 @@
 module.exports = (field, bom) => {
-  // const field1 = [...field];
-  const bom1 = bom;
   for (let i = 0; i < bom.length; i += 1) {
-    const exploded = field.x === bom1[i].x && field.y === bom1[i].y;
-    const field1 = { ...field, exploded };
-
-    if (exploded) {
-      return field1;
-    }
-    return field1;
+    const exploded = field.x === bom[i].x && field.y === bom[i].y;
+    return { ...field, exploded };
   }
   return { x: bom.x, y: bom.y };
 };
