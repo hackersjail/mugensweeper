8b4f1c62c4b17626e1bc68d85bf6a5a5c944f696
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-08 11:04:07 +0900

    axiosのリクエストURLを.envを介して設定可能な形式に改修しました。

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,7 @@ import { URLSearchParams } from 'universal-url';
 export default ({ $axios, store }) => {
   $axios.onRequest((config) => {
     /* eslint-disable no-param-reassign */
-    config.url = `http://localhost:10000/dev${config.url}`; // ToDo:ドメインやディレクトリ構成確定後、ハードコーディング部分修正実施
+    config.url = `${config.baseURL}${config.url}`;
     if (typeof config.data === 'object') {
       const params = new URLSearchParams();
       Object.keys(config.data).forEach((key) => params.append(key, config.data[key]));
