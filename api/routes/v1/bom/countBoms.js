4811121eed89a8c85be6aacc56ff0fc1add2cf25
lilycoco <ry.sep27@gmail.com>
2019-01-10 08:31:11 +0900

    modified2

--- api/routes/v1/bom/countBoms.js
+++ api/routes/v1/bom/countBoms.js
@@ -1,19 +1,7 @@
 const directions = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
 
-module.exports = (bomMap, positions) => {
-  const aroundPositions = [];
-
-  // positionsの周囲8マス
-  const aroundPosition = directions.map(([x, y]) => ({
-    x: x + positions.x,
-    y: y + positions.y,
-  }));
-  aroundPositions.push(...aroundPosition);
-
-  // positionsの周囲8マスにあるBom
-  const leftBlock = aroundPositions.filter((data) =>
-    bomMap.find((d) => d.x === data.x && d.y === data.y),
-  );
-
-  return leftBlock.length;
-};
+module.exports = (bomMap, positions) =>
+  directions.filter((data) =>
+    bomMap.find((d) => d.x === data[0] + positions.x && d.y === data[1] + positions.y),
+  ).length;
+// return leftBlock.length;
