e9fbe83fa3085d8231e8df5df9d8131f94736675
lilycoco <ry.sep27@gmail.com>
2019-01-10 08:31:29 +0900

    modified2

--- api/routes/v1/bom/countBoms.js
+++ api/routes/v1/bom/countBoms.js
@@ -4,4 +4,3 @@ module.exports = (bomMap, positions) =>
   directions.filter((data) =>
     bomMap.find((d) => d.x === data[0] + positions.x && d.y === data[1] + positions.y),
   ).length;
-// return leftBlock.length;
