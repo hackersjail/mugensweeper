1b94c199f17a8ae3034d19cd12cbf54d0af45fdd
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-13 23:01:39 +0900

    if文の箇所についてソースを修正しました。

--- api/routes/v1/user/user.js
+++ api/routes/v1/user/user.js
@@ -6,11 +6,10 @@ router.route('/').post(async (req, res) => {
 
   // 数値、アルファベット、_以外が出たらtrueを返す正規表現
   const halfLetter = /^[a-z0-9]([_a-z0-9]){1,5}[a-z0-9]$/;
-  if (halfLetter.test(userName)) {
-    res.status(200).send(await addUser(userName));
-  } else {
-    res.status(401).send('Error');
-  }
+  /* eslint no-unused-expressions: 0 */
+  halfLetter.test(userName)
+    ? res.status(200).send(await addUser(userName))
+    : res.status(401).send('Error');
 });
 
 module.exports = router;
