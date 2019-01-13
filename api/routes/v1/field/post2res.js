e782a05396ac05de690b668a954310a284f64132
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-14 00:56:38 +0900

    さらにソースを短かくしてみました。

--- api/routes/v1/field/post2res.js
+++ api/routes/v1/field/post2res.js
@@ -4,11 +4,13 @@ const createPartOfBoms = require('../bom/createPartOfBoms.js');
 const judgeExploded = require('../util/judgeExploded.js');
 
 module.exports = (req, field) => {
-  const { actionId } = req;
   const rate = 0.7;
 
   // 周囲へボムを配置
-  createPartOfBoms(rate, field, req).forEach((n) => addBom({ x: n.x, y: n.y, actionId }));
+  createPartOfBoms(rate, field, req).forEach((n) =>
+    addBom({ x: n.x, y: n.y, actionId: n.actionId }),
+  );
+  // 爆弾関連の情報を返却
   return {
     x: req.x,
     y: req.y,
