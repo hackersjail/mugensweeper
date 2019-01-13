bf889d7e31ec30d1af636de74ac2fc5dd79d18c7
Takuto Eto <aoitele@gmail.com>
2019-01-13 15:53:58 +0900

    createBomsを修正

--- api/routes/v1/bom/createPartOfBoms.js
+++ api/routes/v1/bom/createPartOfBoms.js
@@ -1,16 +1,14 @@
 const directions = [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0]];
 
 module.exports = (rate, field, positions) => {
-  const aroundPositions = [];
   const aroundFields = [];
   const boms = [];
 
   // positionsの周囲8マス
-  const aroundPosition = directions.map(([x, y]) => ({
+  const aroundPositions = directions.map(([x, y]) => ({
     x: x + positions.x,
     y: y + positions.y,
   }));
-  aroundPositions.push(...aroundPosition);
 
   // fieldの周囲8マス
   for (let t = 0; t < field.length; t += 1) {
