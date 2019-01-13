7f67a42b1e0926e5f9af26b3952bbe8a5b1985db
lilycoco <ry.sep27@gmail.com>
2019-01-13 17:59:56 +0900

    modified createPart Of Boms

--- api/routes/v1/bom/createPartOfBoms.js
+++ api/routes/v1/bom/createPartOfBoms.js
@@ -4,25 +4,17 @@ module.exports = (rate, field, positions) => {
   const aroundFields = [];
   const boms = [];
 
-  // positionsの周囲8マス
-  const aroundPositions = directions.map(([x, y]) => ({
-    x: x + positions.x,
-    y: y + positions.y,
-  }));
-
   // fieldの周囲8マス
   for (let t = 0; t < field.length; t += 1) {
-    const aroundField = directions.map(([x, y]) => ({
-      x: x + field[t].x,
-      y: y + field[t].y,
-    }));
+    const aroundField = directions.map(([x, y]) => [x + field[t].x, y + field[t].y]);
     aroundFields.push(...aroundField);
   }
 
+  // console.log(a)
   // Bomが置ける余剰
-  const leftBlock = aroundPositions.filter(
-    (data) => !aroundFields.find((d) => d.x === data.x && d.y === data.y),
-  );
+  const leftBlock = directions
+    .map((d) => ({ x: d[0] + positions.x, y: d[1] + positions.y }))
+    .filter((data) => !aroundFields.find((d) => d[0] === data.x && d[1] === data.y));
 
   for (let b = 0; b < leftBlock.length; b += 1) {
     if (Math.random() < rate) {
