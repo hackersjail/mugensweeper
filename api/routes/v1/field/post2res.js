76cfcf63cfdd67575d976d43900b1df13694e4fb
lilycoco <ry.sep27@gmail.com>
2019-01-13 20:04:56 +0900

    modified post2res

--- api/routes/v1/field/post2res.js
+++ api/routes/v1/field/post2res.js
@@ -6,12 +6,14 @@ const judgeExploded = require('../util/judgeExploded.js');
 module.exports = (req, field) => {
   const { x, y, actionId } = req;
   const fieldInfo = { x, y }; // 返却用
+  const rate = +0.7;
 
   // 周囲へボムを配置
-  const result = createPartOfBoms(0.7, field, req);
-  for (let n = 0; n < result.length; n += 1) {
-    addBom({ x: result[n].x, y: result[n].y, actionId });
-  }
+  const result = createPartOfBoms(rate, field, req);
+
+  const add = result.map((n) => ({ x: n.x, y: n.y, actionId }));
+  addBom(add);
+
   // 周囲のボム数を計算
   fieldInfo.bomCount = countBoms(getBom(), req);
   // 爆発するか判定
