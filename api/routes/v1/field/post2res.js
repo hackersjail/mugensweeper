8903ddccad542e5895e134b48eed63b026cc173f
Takuto Eto <aoitele@gmail.com>
2019-01-13 17:56:07 +0900

    post2res修正

--- api/routes/v1/field/post2res.js
+++ api/routes/v1/field/post2res.js
@@ -3,15 +3,15 @@ const countBoms = require('../bom/countBoms.js');
 const createPartOfBoms = require('../bom/createPartOfBoms.js');
 const judgeExploded = require('../util/judgeExploded.js');
 
+const rate = 0.7;
+
 module.exports = (req, field) => {
-  const { x, y, actionId } = req;
+  const { x, y } = req;
   const fieldInfo = { x, y }; // 返却用
 
   // 周囲へボムを配置
-  const result = createPartOfBoms(0.7, field, req);
-  for (let n = 0; n < result.length; n += 1) {
-    addBom({ x: result[n].x, y: result[n].y, actionId });
-  }
+  const add = createPartOfBoms(rate, field, req);
+  addBom(add);
   // 周囲のボム数を計算
   fieldInfo.bomCount = countBoms(getBom(), req);
   // 爆発するか判定
