603eb4492da4619f6e3566683d407a94f12a5f54
NaomiRennie <naomi.lily.405@gmail.com>
2019-01-14 02:44:02 +0900

    add swagger

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 
 router.use('/user', require('./user/user.js'));
 router.use('/field', require('./field/field.js'));
+router.use('/point', require('./point/individualPoint.js'));
+router.use('/ranking', require('./point/ranking.js'));
 
 module.exports = router;
