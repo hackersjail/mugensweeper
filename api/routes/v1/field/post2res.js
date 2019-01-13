b09f1d1ec01bb6ff421de47e7e4d2929cfb03eb6
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-14 00:47:58 +0900

    ソース全体を簡易化しました。

--- api/routes/v1/field/post2res.js
+++ api/routes/v1/field/post2res.js
@@ -4,21 +4,15 @@ const createPartOfBoms = require('../bom/createPartOfBoms.js');
 const judgeExploded = require('../util/judgeExploded.js');
 
 module.exports = (req, field) => {
-  const { x, y, actionId } = req;
-  const fieldInfo = { x, y }; // 返却用
+  const { actionId } = req;
   const rate = 0.7;
 
   // 周囲へボムを配置
   createPartOfBoms(rate, field, req).forEach((n) => addBom({ x: n.x, y: n.y, actionId }));
-
-  // 周囲のボム数を計算
-  fieldInfo.bomCount = countBoms(getBom(), req);
-
-  // 爆発するか判定
-  if (judgeExploded(req, getBom()).exploded) {
-    fieldInfo.exploded = true;
-  } else {
-    fieldInfo.exploded = false;
-  }
-  return fieldInfo;
+  return {
+    x: req.x,
+    y: req.y,
+    bomCount: countBoms(getBom(), req),
+    exploded: judgeExploded(req, getBom()).exploded,
+  };
 };
