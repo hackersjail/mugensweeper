b652d534812cd8b44e35b2c9703f00a108228d6b
NaomiRennie <naomi.lily.405@gmail.com>
2019-01-14 03:08:47 +0900

    point

--- api/routes/v1/point/ranking.js
+++ api/routes/v1/point/ranking.js
@@ -1,20 +1,18 @@
 const router = require('express').Router();
 const fieldStore = require('../../../models/v1/fieldStore.js');
-const bomStore = require('../../../models/v1/bomStore.js');
 const userStore = require('../../../models/v1/userStore.js');
-const { createNewfieldWithBomMap, generateRanking } = require('../../../models/v1/pointStore.js');
+const { generateRanking } = require('../../../models/v1/pointStore.js');
 
 router.post('/', async (req, res) => {
   // fieldの取得
   const field = fieldStore.getData();
-  // bomMapの取得
-  const bomMap = bomStore.getBom();
+  // console.log(field, 'y');
   // userの取得
   const user = userStore.getUser();
-  // new Field with bomMap の作成
-  const fieldWithBoms = createNewfieldWithBomMap(field, bomMap);
+  // console.log(user, 'u');
   // rankingの作成
-  const ranking = generateRanking(fieldWithBoms);
+  const ranking = generateRanking(field);
+  // console.log(ranking, 'ranking');
   const rankingWithUserNames = [];
   for (let i = 0; i < user.length; i += 1) {
     for (let m = 0; m < ranking.length; m += 1) {
@@ -27,6 +25,7 @@ router.post('/', async (req, res) => {
         rankingWithUserNames.push(userPointsWithName);
       }
     }
+    // console.log(rankingWithUserNames);
     res.json(rankingWithUserNames);
   }
 });
--- api/tests/routes/v1/util/ranking.test.js
+++ api/tests/routes/v1/util/ranking.test.js
@@ -1,92 +1,59 @@
-const chai = require('chai');
-const app = require('../../../../routes/app.js');
+// const chai = require('chai');
+// const app = require('../../../../routes/app.js');
 // const array2fieldHistory = require('./array2fieldHistory.js');
-const { initData, getData, addData, saveData } = require('../../../../models/v1/fieldStore.js');
-// const FieldHistoryModel = require('../../../../models/v1/FieldHistoryModel.js');
+// const { initData, getData, addData, saveData } = require('../../../../models/v1/fieldStore.js');
+// const { initUser, getUser, addUser, saveUser } = require('../../../../models/v1/userStore.js');
+const FieldHistoryModel = require('../../../../models/v1/FieldHistoryModel.js');
+const UserModel = require('../../../../models/v1/UserModel.js');
 // const {
 //   createNewfieldWithBomMap,
 //   calculatePointsForPlayer,
 //   generateRanking,
 // } = require('../../../../models/v1/pointStore.js');
-const ranking = require('../../../../routes/v1/point/ranking.js');
+// const ranking = require('../../../../routes/v1/point/ranking.js');
 const { connectDB, disconnectDB, dropDB } = require('../../../../database.js');
 
 // const t = Math.round(new Date().getTime() / 1000);
 
 describe('ブロックを開くとき', () => {
   beforeAll(connectDB);
-  beforeEach(initData);
+  // beforeEach(initData);
   afterEach(dropDB);
   afterAll(disconnectDB);
   it('得点に関するテスト', async () => {
     // Given
-    // prettier-ignore
-    // const fieldinfo = array2fieldHistory([
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, { t, u: 1, f: 7 }, { t, u: 3, f: 8 }, { t, u: 4, f: 13 }, 0,
-    //   0, 0, 0, 0, { t, u: 2, f: 2 }, { t, u: 1, f: 6 }, 0, { t, u: 3, f: 9 }, 0,
-    //   0, 0, 0, 0, { t, u: 1, f: 1 }, { t, u: 2, f: 5 }, { t, u: 2, f: 4 }, { t, u: 4, f: 10 }, { t, u: 5, f: 12 },
-    //   0, 0, 0, 0, 0, { t, u: 1, f: 3 }, { t, u: 1, f: 11 }, 0, { t, u: 1, f: 14 },
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // ]);
-    const fieldinfo1 = [{x:1,y:1,userId:1,actionId:1},{x:1,y:2,userId:2,actionId:2},{x:1,y:3,userId:1,actionId:3}];
-    // const fieldinfo = [
-    //   { x: 2, y: 1, userId: 1, actionId: 4 },
-    //   { x: 2, y: 2, userId: 2, actionId: 5 },
-    //   { x: 2, y: 3, userId: 1, actionId: 6 },
-    // ];
-    // const fieldinfo = [{x:1,y:1},{x:1,y:2},{x:1,y:3}];
 
-    // // prettier-ignore
-    // const bomMap = array2fieldHistory([
-    //   0, 0, 0, 0, 0, { f: 7 }, 0, { f: 8 }, { f: 13 },
-    //   0, 0, 0, 0, { f: 2 }, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, { f: 1 }, 0, { f: 4 }, { f: 12 },
-    //   0, 0, 0, 0, 0, { f: 3 }, 0, 0, 0,
-    //   0, 0, 0, 0, 0, 0, 0, { f: 10 }, 0,
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    //   0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // ]);
+    const fieldinfo1 = [
+      { x: 1, y: 1, userId: 1, actionId: 1 },
+      { x: 1, y: 2, userId: 2, actionId: 2 },
+      { x: 1, y: 3, userId: 1, actionId: 3 },
+    ];
+    const userinfo = [{ userName: 'Nanako', userId: 1 }, { userName: 'Taro', userId: 2 }];
+    await FieldHistoryModel.insertMany(fieldinfo1);
+    await UserModel.insertMany(userinfo);
+    // await initData();
+    // await initUser();
 
-    // const clicked = [
-    //   { userId: 1, points: 3 },
-    //   { userId: 4, points: 2 },
-    //   { userId: 5, points: 1 },
-    //   { userId: 2, points: 0 },
-    //   { userId: 3, points: 0 },
-    // ];
+    // console.log(a);
 
-    // const rankingMatcher = [
-    //   { userId: 1, points: 3 },
-    //   { userId: 4, points: 2 },
-    //   { userId: 5, points: 1 },
-    //   { userId: 2, points: 0 },
-    //   { userId: 3, points: 0 },
-    // ];
-    // const beforePostField = await FieldHistoryModel.insertMany(fieldinfo1);
+    // const data = getData();
+    // console.log(data);
 
-    const data = getData();
+    // let lastBody;
+    // for (let i = 0; i < fieldinfo1.length; i += 1) {
+    //   const { field } = fieldinfo1[i];
+    //   const { body } = await chai
+    //     .request(app)
+    //     .post('/v1/ranking')
+    //     .set('content-type', 'application/x-www-form-urlencoded')
+    //     .send({ field });
+    // lastBody = body;
+    // }
+    // console.log(lastBody);
 
-    let lastBody;
-    for (let i = 0; i < fieldinfo1.length; i += 1) {
-      const { field } = fieldinfo1[i];
-      const { body } = await chai
-        .request(app)
-        .post('/v1/field')
-        .set('content-type', 'application/x-www-form-urlencoded')
-        .send({ field });
-      lastBody = body;
-    }
-    addData(lastBody);
-    await saveData();
     // const data = getData();
 
-    expect(ranking).toEqual(data);
+    // expect(ranking).toEqual(data);
 
     // // fieldHistoryとbomHistoryを使って爆弾情報を含んだfieldの生成
     // const field2 = createNewfieldWithBomMap(fieldinfo, bomMap);
