a572c0f0253bb7f15ed3f572c548c8587f136c8a
lilycoco <ry.sep27@gmail.com>
2019-01-14 20:26:45 +0900

    modified

--- api/models/v1/pointStore.js
+++ api/models/v1/pointStore.js
@@ -2,14 +2,16 @@ const judgeExploded = require('../../routes/v1/util/judgeExploded.js');
 
 const eachPoints = (fieldInfo) =>
   fieldInfo.reduce((sortedField, block) => {
-    let users = [{ userId: block.userId, points: 1 }, ...sortedField];
+    let userInfo = [{ userId: block.userId, points: 1 }, ...sortedField];
     if (block.exploded || sortedField.findIndex((a) => a.userId === block.userId) !== -1) {
-      users[0].points = block.exploded
+      userInfo[0].points = block.exploded
         ? 0
         : sortedField.find((a) => a.userId === block.userId).points + 1;
-      users = users.filter((v1, i1, a1) => a1.findIndex((v2) => v1.userId === v2.userId) === i1);
+      userInfo = userInfo.filter(
+        (v1, i1, a1) => a1.findIndex((v2) => v1.userId === v2.userId) === i1,
+      );
     }
-    return users;
+    return userInfo;
   }, []);
 
 module.exports = {
