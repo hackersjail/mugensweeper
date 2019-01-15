f1d25e2eb54faa7d9f75e7e1c63efb395c7f8794
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-15 13:36:01 +0900

    最終版になります。

--- api/routes/v1/util/judgeField.js
+++ api/routes/v1/util/judgeField.js
@@ -1,14 +1,14 @@
 const directions = require('../../../util/directions.js')();
 
 module.exports = (add, field) => {
+  let existBlock = true;
   let newBlock = false;
   const newDirs = directions.map((c) => [+add.x + c[0], +add.y + c[1]]);
-  for (let i = 0; i < field.length; i += 1) {
+  field.forEach((d) => {
     // 既に開いたblockを再度開こうとした場合はfalseを返却
-    if (field[i].x === +add.x && field[i].y === +add.y) return false;
+    if (existBlock && d.x === +add.x && d.y === +add.y) existBlock = false;
     // 既に開いたblockの周囲8blockを開こうとした場合はtrueを返却
-    if (!newBlock && newDirs.find((data) => field[i].x === data[0] && field[i].y === data[1]))
-      newBlock = true;
-  }
-  return newBlock;
+    if (!newBlock && newDirs.find((data) => d.x === data[0] && d.y === data[1])) newBlock = true;
+  });
+  return existBlock && newBlock;
 };
