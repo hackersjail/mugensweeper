41a8cce50c7c1303e03a21829c0554ae4ca7b00b
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-15 13:15:27 +0900

    newBlockが開いた場合にif文を処理しない形式に修正しました。

--- api/routes/v1/util/judgeField.js
+++ api/routes/v1/util/judgeField.js
@@ -2,12 +2,13 @@ const directions = require('../../../util/directions.js')();
 
 module.exports = (add, field) => {
   let newBlock = false;
-  const newDirs = directions.map((c) => [c[0] + +add.x, c[1] + +add.y]);
+  const newDirs = directions.map((c) => [+add.x + c[0], +add.y + c[1]]);
   for (let i = 0; i < field.length; i += 1) {
     // 既に開いたblockを再度開こうとした場合はfalseを返却
     if (field[i].x === +add.x && field[i].y === +add.y) return false;
     // 既に開いたblockの周囲8blockを開こうとした場合はtrueを返却
-    if (newDirs.find((data) => field[i].x === data[0] && field[i].y === data[1])) newBlock = true;
+    if (!newBlock && newDirs.find((data) => field[i].x === data[0] && field[i].y === data[1]))
+      newBlock = true;
   }
   return newBlock;
 };
