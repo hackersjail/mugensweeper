7545bf2447533a5cbd431be33e5597896f635025
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-01-15 09:29:39 +0900

    Update Swagger configuration

--- api/swagger.js
+++ api/swagger.js
@@ -1,11 +1,11 @@
 const swaggerJSDoc = require('swagger-jsdoc');
-const { HOST } = require('./config.js');
+const { NODE_ENV, HOST, PORT } = require('./config.js');
 
 const customSiteTitle = 'MugenSweeper API';
 const versions = {
   v1: {
     title: customSiteTitle,
-    version: '0.0.1',
+    version: process.env.npm_package_version,
     description: `Document for MugenSweeper API`,
   },
   dev: {
@@ -20,12 +20,12 @@ module.exports = Object.keys(versions).map((version) => ({
     swaggerDefinition: {
       swagger: '2.0',
       info: versions[version],
-      host: HOST,
+      host: NODE_ENV === 'development' ? `${HOST}:${PORT}` : HOST,
       basePath: `/${version}`,
       consumes: ['application/x-www-form-urlencoded'],
       produces: ['application/json'],
     },
-    apis: [`./api/docs/${version}/*.yml`],
+    apis: [`./api/docs/${version}/**/*.yml`],
   }),
   option: {
     customSiteTitle,
