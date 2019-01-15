3293f2621cd282da2dee3af550fed8d23cf0bfd3
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-15 12:59:44 +0900

    for文の処理に修正し直しました。

--- api/routes/v1/util/judgeField.js
+++ api/routes/v1/util/judgeField.js
@@ -1,12 +1,13 @@
 const directions = require('../../../util/directions.js')();
 
 module.exports = (add, field) => {
-  let flag1 = true;
-  let flag2 = false;
-  field.forEach((d) => {
-    if (d.x === +add.x && d.y === +add.y) flag1 = false;
-    if (directions.find((data) => d.x === data[0] + +add.x && d.y === data[1] + +add.y))
-      flag2 = true;
-  });
-  return flag1 && flag2;
+  let newBlock = false;
+  const newDirs = directions.map((c) => [c[0] + +add.x, c[1] + +add.y]);
+  for (let i = 0; i < field.length; i += 1) {
+    // 既に開いたblockを再度開こうとした場合はfalseを返却
+    if (field[i].x === +add.x && field[i].y === +add.y) return false;
+    // 既に開いたblockの周囲8blockを開こうとした場合はtrueを返却
+    if (newDirs.find((data) => field[i].x === data[0] && field[i].y === data[1])) newBlock = true;
+  }
+  return newBlock;
 };
