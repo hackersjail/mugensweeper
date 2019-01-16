43e46b80a3f8215ef43008d4faee5f4ee9f48c04
lilycoco <ry.sep27@gmail.com>
2019-01-17 06:45:38 +0900

    modified

--- api/routes/v1/bomb/createPartOfBombs.js
+++ api/routes/v1/bomb/createPartOfBombs.js
@@ -1,17 +1,15 @@
 const directions = require('../../../util/directions.js')();
 
-module.exports = (rate, field, position) => {
-  const aroundFields = [];
-
-  // fieldの周囲8マス
-  for (let t = 0; t < field.length; t += 1) {
-    const aroundField = directions.map(([x, y]) => [x + field[t].x, y + field[t].y]);
-    aroundFields.push(...aroundField);
-  }
-
+module.exports = (rate, field, position) =>
   // Bombが置ける余剰をrateの確立で返す
-  return directions
-    .map((around) => ({ x: around[0] + position.x, y: around[1] + position.y }))
-    .filter((data) => !aroundFields.find((f) => f[0] === data.x && f[1] === data.y))
+  directions
+    .filter(
+      (data) =>
+        !field.filter(
+          (d) =>
+            (data[0] + position.x === d.x && data[1] + position.y === d.y) ||
+            (data[0] + position.x === data[0] + d.x && data[1] + +position.y === data[1] + d.y),
+        ),
+    )
+    .map(([x, y]) => ({ x, y }))
     .filter(() => Math.random() < rate);
-};
