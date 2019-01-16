5e179235f46e7782eb9a040b579c8ce9ebc297e8
lilycoco <ry.sep27@gmail.com>
2019-01-17 07:37:03 +0900

    modified

--- api/routes/v1/bomb/createPartOfBombs.js
+++ api/routes/v1/bomb/createPartOfBombs.js
@@ -1,15 +1,14 @@
 const directions = require('../../../util/directions.js')();
 
+// Bombが置ける余剰をrateの確立で返す
 module.exports = (rate, field, position) =>
-  // Bombが置ける余剰をrateの確立で返す
   directions
     .map(([x, y]) => ({ x, y }))
     .filter(
       (data) =>
         !field.filter(
           (d) =>
-            ((data.x + position.x === d.x && data.y + position.y === d.y) ||
-              (data.x + position.x === data.x + d.x && data.y + +position.y === data.y + d.y)) &&
-            Math.random() < rate,
-        ),
+            (data.x + position.x === d.x && data.y + position.y === d.y) ||
+            (data.x + position.x === data.x + d.x && data.y + +position.y === data.y + d.y),
+        ) && Math.random() < rate,
     );
