c03da2cdab8d8b5f53a6444288c93d86af239812
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-19 08:22:50 +0900

    爆弾blockを開いた際、周囲の爆弾個数表示が優先して表示されるバグを修正しました。

--- pages/index.vue
+++ pages/index.vue
@@ -170,7 +170,7 @@ export default {
           this.gridWidth * block.x -
           this.gridWidth / 2 -
           this.moveDist.x}px`,
-        backgroundPosition: `${block.bombCount !== 0 ? (block.bombCount - 1) * -30 : -301}px 0px`,
+        backgroundPosition: `${!block.exploded ? (block.bombCount - 1) * -30 : -301}px 0px`,
       };
     },
     onTouchStart(e) {
