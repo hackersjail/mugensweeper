ff71bc1ab1a32d49a160f89f946dd34bf8c2c096
Dmiyamoto <m2985.damiyamoto@gmail.com>
2019-01-20 17:22:41 +0900

    リクエストヘッダー上のtoken有無に関するテストを追加しました。

--- api/tests/routes/v1/field/field.test.js
+++ api/tests/routes/v1/field/field.test.js
@@ -8,6 +8,8 @@ const { connectDB, disconnectDB, dropDB } = require('../../../../database.js');
 const ZERO00000 = 0;
 const FIRST_ONE = 'u0:0:op';
 const propFilter = '-_id -__v';
+const token =
+  'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJleGFtcGxlMCIsImV4cCI6MTcwNTc0MjM4OSwiYXVkIjoibXVnZW5zd2VlcGVycyIsImlzcyI6Im11Z2Vuc3dlZXBlcnMifQ.ISinmaTZDcOzHM4q4MALgmnSA7x6wN9pa1wkRGvlRWw';
 
 describe('field情報を返せるかどうか', () => {
   beforeAll(connectDB);
@@ -41,6 +43,87 @@ describe('field情報を返せるかどうか', () => {
     expect(fieldHistory).toEqual(expect.arrayContaining(fieldHistory2));
   });
 
+  it('フロント側からのHTTPリクエストヘッダーにサーバ側userStore内に存在するuserIdに対応したtokenが存在しない場合の動作テスト', async () => {
+    // Given
+    // prettier-ignore
+    const fieldHistory = array2fieldHistory([
+        ZERO00000, ZERO00000, ZERO00000, 'u2:5:op', 'u1:4:op',
+        ZERO00000, ZERO00000, ZERO00000, 'u3:3:op', ZERO00000,
+        ZERO00000, ZERO00000, FIRST_ONE, ZERO00000, ZERO00000,
+        ZERO00000, ZERO00000, 'u2:1:op', ZERO00000, ZERO00000,
+        ZERO00000, 'u1:2:op', ZERO00000, ZERO00000, ZERO00000,
+      ]);
+
+    // prettier-ignore
+    const add = array2fieldHistory([
+        ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000,
+        ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, 'u2:6:op',
+        ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000,
+        ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000,
+        ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000,
+        ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000,
+        ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000, ZERO00000,
+      ]);
+
+    // When
+    await FieldHistoryModel.insertMany(fieldHistory);
+    await initData();
+    const beforePostField = getData();
+
+    const noTokenGet = (await chai.request(app).get('/v1/field')).body;
+
+    const withTokenGet = (await chai
+      .request(app)
+      .get('/v1/field')
+      .set('Authorization', token)).body; // headerにテスト用tokenセット
+
+    await chai
+      .request(app)
+      .post('/v1/field')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send(add[0]);
+
+    const noTokenPost = (await chai
+      .request(app)
+      .get('/v1/field')
+      .set('Authorization', token)).body; // headerにテスト用tokenセット
+
+    await chai
+      .request(app)
+      .post('/v1/field')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .set('Authorization', token) // headerにテスト用tokenセット
+      .send(add[0]);
+
+    const withTokenPost = (await chai
+      .request(app)
+      .get('/v1/field')
+      .set('Authorization', token)).body; // headerにテスト用tokenセット
+
+    // Then
+    const rsMatchers = [
+      { x: 0, y: 0 },
+      { x: 0, y: -1 },
+      { x: -1, y: -2 },
+      { x: 1, y: 1 },
+      { x: 2, y: 2 },
+      { x: 1, y: 2 },
+      { x: 3, y: 2 },
+    ];
+
+    // ・Response
+    const result = [];
+    for (let n = 0; n < withTokenPost.length; n += 1) {
+      result.push({ x: +withTokenPost[n].x, y: +withTokenPost[n].y });
+      expect(withTokenPost[n]).toHaveProperty('exploded');
+      expect(withTokenPost[n]).toHaveProperty('bombCount');
+    }
+    expect(noTokenGet).toEqual({}); // ヘッダーにtoken無しでgetした場合の確認
+    expect(withTokenGet).toHaveLength(beforePostField.length); // ヘッダーにtoken有りでgetした場合の確認
+    expect(noTokenPost).toHaveLength(beforePostField.length); // ヘッダーにtoken無しでpost後にgetした場合の確認
+    expect(result).toEqual(expect.arrayContaining(rsMatchers)); // ヘッダーにtoken有りでpost後にgetした場合の確認
+  });
+
   it('DBにfieldHistoryを追加するテスト', async () => {
     // Given
     // prettier-ignore
@@ -67,6 +150,7 @@ describe('field情報を返せるかどうか', () => {
     const dupli = { x: 2, y: 2, userId: 4, actionId: 9, action: 'opened', status: false };
 
     // When
+    await FieldHistoryModel.deleteMany();
     await FieldHistoryModel.insertMany(fieldHistory);
     await initData();
     const beforePostField = getData();
@@ -81,22 +165,14 @@ describe('field情報を返せるかどうか', () => {
         .request(app)
         .post('/v1/field')
         .set('content-type', 'application/x-www-form-urlencoded')
-        // テスト用token
-        .set(
-          'Authorization',
-          'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJleGFtcGxlMCIsImV4cCI6MTcwNTc0MjM4OSwiYXVkIjoibXVnZW5zd2VlcGVycyIsImlzcyI6Im11Z2Vuc3dlZXBlcnMifQ.ISinmaTZDcOzHM4q4MALgmnSA7x6wN9pa1wkRGvlRWw',
-        )
+        .set('Authorization', token) // headerにテスト用tokenセット
         .send(add[i]);
     }
 
     const { body } = await chai
       .request(app)
       .get('/v1/field')
-      // テスト用token
-      .set(
-        'Authorization',
-        'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJleGFtcGxlMCIsImV4cCI6MTcwNTc0MjM4OSwiYXVkIjoibXVnZW5zd2VlcGVycyIsImlzcyI6Im11Z2Vuc3dlZXBlcnMifQ.ISinmaTZDcOzHM4q4MALgmnSA7x6wN9pa1wkRGvlRWw',
-      );
+      .set('Authorization', token); // headerにテスト用tokenセット
 
     // Then
     const rsMatchers = [
