99b9ccc469e36688380f89d0b1f81cb26f41e629
lilycoco <ry.sep27@gmail.com>
2019-01-21 13:37:59 +0900

    refacter judgeField.js

--- api/routes/v1/util/judgeField.js
+++ api/routes/v1/util/judgeField.js
@@ -1,4 +1,4 @@
 module.exports = (add, field) => {
-  const len = field.filter((d) => Math.abs(d.x - add.x) <= 1 && Math.abs(d.y - add.y) <= 1);
-  return len.length ? !len.find((f) => f.x === +add.x && f.y === +add.y) : false;
+  const around = field.filter((d) => Math.abs(d.x - add.x) <= 1 && Math.abs(d.y - add.y) <= 1);
+  return around.length ? !around.find((f) => f.x === +add.x && f.y === +add.y) : false;
 };
