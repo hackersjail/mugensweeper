1e8e89ad41c179c7d460ff5f2be572a2f7a121d0
lilycoco <ry.sep27@gmail.com>
2019-01-22 07:27:11 +0900

    modified

--- api/routes/v1/util/judgeField.js
+++ api/routes/v1/util/judgeField.js
@@ -1,4 +1,4 @@
 module.exports = (add, field) => {
   const around = field.filter((d) => Math.abs(d.x - add.x) <= 1 && Math.abs(d.y - add.y) <= 1);
-  return around.length ? !around.find((f) => f.x === +add.x && f.y === +add.y) : false;
+  return !!around.length && !around.find((f) => f.x === +add.x && f.y === +add.y);
 };
