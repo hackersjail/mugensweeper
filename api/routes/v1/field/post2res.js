e98f66d18f6dbeef7967d91d4f554d5611cbed93
Takuto Eto <aoitele@gmail.com>
2019-01-26 02:45:37 +0900

    post2resを元に戻しました

--- api/routes/v1/field/post2res.js
+++ api/routes/v1/field/post2res.js
@@ -1,13 +1,21 @@
-const { getBomb } = require('../../../models/v1/bombStore.js');
+const { getBomb, addBomb } = require('../../../models/v1/bombStore.js');
 const countBombs = require('../bomb/countBombs.js');
+const createPartOfBombs = require('../bomb/createPartOfBombs.js');
 const judgeExploded = require('../util/judgeExploded.js');
 
-module.exports = (req) =>
+module.exports = (req, field) => {
+  const rate = 0.7;
+
+  // 周囲へボムを配置
+  createPartOfBombs(rate, field, req).forEach((n) =>
+    addBomb({ x: n.x, y: n.y, actionId: n.actionId }),
+  );
   // 爆弾関連の情報を返却
-  ({
+  return {
     x: +req.x,
     y: +req.y,
     bombCount: countBombs(getBomb(), req),
     exploded: judgeExploded(req, getBomb()).exploded,
     userId: req.userId,
-  });
+  };
+};
--- api/routes/v1/util/judgeExploded.js
+++ api/routes/v1/util/judgeExploded.js
@@ -1,4 +1,4 @@
 module.exports = (block, bomb) => {
-  const match = bomb.find((bombBlock) => +bombBlock.x === block.x && +bombBlock.y === block.y);
+  const match = bomb.find((bombBlock) => bombBlock.x === +block.x && bombBlock.y === +block.y);
   return { ...block, exploded: match !== undefined };
 };
