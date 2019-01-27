8dd068edba88f6d897d924a4fa79b578b9bb629b
NaomiRennie <naomi.lily.405@gmail.com>
2019-01-27 14:08:13 +0900

    ranking5 1/27

--- api/routes/v1/point/ranking.js
+++ api/routes/v1/point/ranking.js
@@ -3,13 +3,33 @@ const fieldStore = require('../../../models/v1/fieldStore.js');
 const userStore = require('../../../models/v1/userStore.js');
 const generateRankingWithUserNames = require('../util/generateRankingWithUserNames.js');
 
-router.post('/', async (req, res) => {
+const bestfive = [];
+
+router.get('/', async (req, res) => {
+  // console.log('YYYYY');
+  // const { userId } = jwt.decode(accessToken);
+  // console.log(userId);
+  // const user1 = req.currentUser;
   // fieldの取得
   const field = await fieldStore.getData();
   // userの取得
   const user = await userStore.getUser();
   // rankingの作成
   const ranking = generateRankingWithUserNames(field, user);
-  res.json(ranking);
+
+  await ranking.sort((a, b) => b.points - a.points);
+  // console.log(ranking);
+  for (let i = 0; i < ranking.length; i += 1) {
+    if (bestfive.length < 5) {
+      bestfive.push(ranking[i]);
+    } else if (bestfive.length <= 5) {
+      if (ranking[5].points === ranking[i].points) {
+        bestfive.push(ranking[i]);
+      } else {
+        break;
+      }
+    }
+  }
+  res.json(bestfive);
 });
 module.exports = router;
--- api/tests/routes/v1/util/ranking.test.js
+++ api/tests/routes/v1/util/ranking.test.js
@@ -26,29 +26,41 @@ describe('ブロックを開くとき', () => {
     // param means 'u2:5:op' → userid:2, order:5, action:opened
     // action pattern → op:opened, **unset(sf:setFlag, df deleteFlag)
     const fieldHistory = array2fieldHistory([
-      ZERO00000, ZERO00000, ZERO00000, 'u2:5:op', 'u1:4:op',
-      ZERO00000, ZERO00000, ZERO00000, 'u3:3:op', ZERO00000,
-      ZERO00000, ZERO00000, FIRST_ONE, ZERO00000, ZERO00000,
-      ZERO00000, ZERO00000, 'u2:1:op', ZERO00000, ZERO00000,
-      ZERO00000, 'u1:2:op', ZERO00000, ZERO00000, ZERO00000,
+      ZERO00000,ZERO00000,ZERO00000,ZERO00000,ZERO00000,ZERO00000,ZERO00000,
+      ZERO00000,ZERO00000, ZERO00000, ZERO00000, 'u2:5:op', 'u1:4:op',ZERO00000,
+      ZERO00000,'u3:12:op', 'u5:7:op', ZERO00000, 'u3:3:op', ZERO00000,ZERO00000,
+      ZERO00000,'u6:11:op', 'u5:10:op', FIRST_ONE, 'u4:6:op', 'u5:9:op',ZERO00000,
+      ZERO00000,'u3:13:op', ZERO00000, 'u2:1:op', 'u4:8:op', ZERO00000,ZERO00000,
+      ZERO00000,ZERO00000, 'u1:2:op', ZERO00000, ZERO00000, ZERO00000,ZERO00000,
+      ZERO00000,ZERO00000,'u7:14:op',ZERO00000,ZERO00000,ZERO00000,ZERO00000,
     ], time);
     // prettier-ignore
     const bombHistory = array2bomMap([
-      0, 0, 5, 4, 0,
-      0, 0, 0, 0, 3,
-      0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0,
-      0, 1, 2, 0, 0,
+      0, 0, 0, 0, 0, 0, 0,
+      0, 7, 5, 4, 0, 0, 0,
+      12, 0, 0, 0, 3, 0, 0,
+      11, 0, 0, 0, 6, 0, 0,
+      13, 0, 10, 0, 8, 9, 0,
+      0, 1, 2, 0, 0, 0, 0,
+      0, 14, 0, 0, 0, 0, 0,
     ], time);
     const userinfo = [
       { userName: 'Nanako', userId: 1 },
       { userName: 'Taro', userId: 2 },
       { userName: 'Ken', userId: 3 },
+      { userName: 'Jiro', userId: 4 },
+      { userName: 'Momoko', userId: 5 },
+      { userName: 'Sayaka', userId: 6 },
+      { userName: 'Yuki', userId: 7 },
     ];
+    // const currentUser = { userName: 'Nanako', userId: 1 };
     const matcher = [
-      { points: 1, userId: '3', userName: 'Ken' },
+      { points: 3, userId: '5', userName: 'Momoko' },
+      { points: 2, userId: '3', userName: 'Ken' },
+      { points: 2, userId: '2', userName: 'Taro' },
       { points: 1, userId: '1', userName: 'Nanako' },
-      { points: 0, userId: '2', userName: 'Taro' },
+      { points: 1, userId: '6', userName: 'Sayaka' },
+      { points: 1, userId: '7', userName: 'Yuki' },
     ];
 
     await BombHistoryModel.insertMany(bombHistory);
@@ -60,10 +72,20 @@ describe('ブロックを開くとき', () => {
     await initData();
     await initUser();
 
+    // const data = getData();
+    // console.log(data);
+
     const { body } = await chai
       .request(app)
-      .post('/v1/point')
+      .get('/v1/point')
       .set('content-type', 'application/x-www-form-urlencoded');
+
+    // const { currentUserInfo } = await chai
+    //   .request(app)
+    //   .get('/v1/point')
+    //   .set('content-type', 'application/x-www-form-urlencoded')
+    //   // .set('Authorization', token) // headerにテスト用tokenセット
+    //   .send(currentUser);
 
     expect(body).toEqual(matcher);
   });
